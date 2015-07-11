53648fdc9fb0cb6b9ac14ef53f09af9031fc1486
Christian Panse <cp@fgcz.ethz.ch>
2015-07-11 16:28:57 +0200

    added xsd check added make test

--- apps/scaffold/fgcz_scaffold_create_xml_driver.py
+++ apps/scaffold/fgcz_scaffold_create_xml_driver.py
@@ -9,6 +9,7 @@ import fileinput
 import os.path
 import sys
 import getopt
+import urllib
 
 #import xml.dom.minidom as minidom
 from lxml import etree 
@@ -19,18 +20,95 @@ $Id: fgcz_scaffold_create_xml_driver.py 7552 2015-06-30 11:21:26Z cpanse $
 $Date: 2015-06-30 13:21:26 +0200 (Tue, 30 Jun 2015) $
 """
 
+
+scaffold_xsd = """
+<xs:schema attributeFormDefault="unqualified" elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <xs:element name="Scaffold">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element name="Experiment">
+          <xs:complexType>
+            <xs:sequence>
+              <xs:element name="FastaDatabase">
+                <xs:complexType>
+                  <xs:simpleContent>
+                    <xs:extension base="xs:string">
+                      <xs:attribute type="xs:string" name="id"/>
+                      <xs:attribute type="xs:string" name="path"/>
+                      <xs:attribute type="xs:string" name="databaseAccessionRegEx"/>
+                      <xs:attribute type="xs:string" name="databaseDescriptionRegEx"/>
+                      <xs:attribute type="xs:string" name="decoyProteinRegEx"/>
+                    </xs:extension>
+                  </xs:simpleContent>
+                </xs:complexType>
+              </xs:element>
+              <xs:element name="BiologicalSample">
+                <xs:complexType>
+                  <xs:sequence>
+                    <xs:element type="xs:string" name="InputFile" maxOccurs="1500" minOccurs="0"/>
+                  </xs:sequence>
+                  <xs:attribute type="xs:string" name="database"/>
+                  <xs:attribute type="xs:boolean" name="analyzeAsMudpit"/>
+                  <xs:attribute type="xs:string" name="name"/>
+                  <xs:attribute type="xs:string" name="category"/>
+                </xs:complexType>
+              </xs:element>
+              <xs:element name="DisplayThresholds">
+                <xs:complexType>
+                  <xs:simpleContent>
+                    <xs:extension base="xs:string">
+                      <xs:attribute type="xs:string" name="name"/>
+                      <xs:attribute type="xs:string" name="id"/>
+                      <xs:attribute type="xs:float" name="proteinProbability"/>
+                      <xs:attribute type="xs:byte" name="minimumPeptideCount"/>
+                      <xs:attribute type="xs:float" name="peptideProbability"/>
+                    </xs:extension>
+                  </xs:simpleContent>
+                </xs:complexType>
+              </xs:element>
+              <xs:element name="Export">
+                <xs:complexType>
+                  <xs:simpleContent>
+                    <xs:extension base="xs:string">
+                      <xs:attribute type="xs:string" name="type"/>
+                      <xs:attribute type="xs:string" name="thresholds"/>
+                      <xs:attribute type="xs:string" name="path"/>
+                    </xs:extension>
+                  </xs:simpleContent>
+                </xs:complexType>
+              </xs:element>
+            </xs:sequence>
+            <xs:attribute type="xs:string" name="name"/>
+            <xs:attribute type="xs:boolean" name="analyzeWithTandem"/>
+            <xs:attribute type="xs:boolean" name="connectToNCBI"/>
+            <xs:attribute type="xs:boolean" name="condenseDataWhileLoading"/>
+            <xs:attribute type="xs:boolean" name="annotateWithGOA"/>
+            <xs:attribute type="xs:string" name="unimodFile"/>
+            <xs:attribute type="xs:boolean" name="highMassAccuracyScoring"/>
+          </xs:complexType>
+        </xs:element>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+</xs:schema>
+"""
+
 # MAIN
 
 def main(argv):
+    """
+    """
+
     tandemOption = 'false'
     mudPitOption = 'false'
+    gelcmsOption = 'false'
     workunitId = None
     qModel = None
     qModelSpace = ['iTRAQ_4-Plex', 'iTRAQ_8-Plex']
     try:
-        opts, args = getopt.getopt(argv, "m:x:q:w:", ["mudpit=", "xtandem=", "qmodel=", "workunit="])
+        opts, args = getopt.getopt(argv, "m:x:q:w:g:", ["mudpit=", "xtandem=", "qmodel=", "workunit=", "gelcms="])
     except getopt.GetoptError:
-        print "{0} --mudpit=[TRUE|FALSE] --xtandem=[TRUE|FALSE]".format(sys.argv[0])
+        print "{0} --mudpit=[TRUE|FALSE] --xtandem=[TRUE|FALSE] --gelcms=[TRUE|FALSE]".format(sys.argv[0])
         sys.exit(1)
 
     for opt, arg in opts:
@@ -38,7 +116,11 @@ def main(argv):
             if arg in ['TRUE', 'true', 'True']:
                 mudPitOption='true'
         elif opt in ("-x", "--xtandem"):
+            if arg in ['TRUE', 'true', 'True']:
                 tandemOption='true'
+        elif opt in ("-g", "--gelcms"):
+            if arg in ['TRUE', 'true', 'True']:
+                gelcmsOption='true'
         elif opt in ("-w", "--workunit"):
                 workunitId=arg
         elif opt in ("-q", "--qmodel"):
@@ -53,7 +135,7 @@ def main(argv):
     scaffoldOutputFilePath = "{0}/scaffold.sf3".format(os.getcwd())
 
     for line in fileinput.input(files=['-']):
-        f = line.rstrip('\n')
+        f = os.path.basename(line.rstrip('\n'))
         f = os.path.abspath(f)
         if os.path.isfile(f):
             inputFiles.append(f)
@@ -81,7 +163,7 @@ def main(argv):
         if i > 1500: 
             break
 
-    # todo(cp): should be workuntit ID
+    # TODO(cp): should be workuntit ID
     experimentName = "Experiment: {0}".format(searchTitle)
     if workunitId is not None:
         experimentName = "Experiment: FGCZ B-Fabric workunit id {0}".format(workunitId)
@@ -201,13 +283,20 @@ def main(argv):
 
     rough_string = etree.tostring(root, pretty_print=True)
 
-    # double check if xml string can be parsed
+    # TODO(cp): xsd check would be nice to have
     try:
-        parser = etree.XMLParser(remove_blank_text=True)
-        #xmlScaffoldDriver = etree.parse("driver.xml", parser)
-        xmlScaffoldDriver = etree.fromstring(rough_string, parser)
+        schema = etree.XMLSchema(etree.XML(scaffold_xsd))
     except:
+        print "build xsd schema failed."
+        raise
+
+    # double check if xml string can be parsed
+    parser = etree.XMLParser(remove_blank_text=True, schema=schema)
+    try:
+        xmlScaffoldDriver = etree.fromstring(rough_string, parser)
+    except ValueError, e:
         print "error: scaffold driver xml can not be parsed"
+        print e
         sys.exit(1)
 
     # print results - write to /dev/stdout 
