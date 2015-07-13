2230e001f95cc34e65d6ab28e53052dacaca42d2
Christian Panse <cp@fgcz.ethz.ch>
2015-07-13 10:06:46 +0200

    code cosmetics

--- apps/scaffold/fgcz_scaffold_create_xml_driver.py
+++ apps/scaffold/fgcz_scaffold_create_xml_driver.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python2.7
 
-# Creates scaffoldBatch driver for a list of dat files.
-# usage: python create_scaffold_xml_driver.py scaffoldOutputFilePath.sf3 myDat1.dat myDat2.dat...
-
-
+"""
+Creates scaffoldBatch driver for a list of dat files.
+usage: python create_scaffold_xml_driver.py scaffoldOutputFilePath.sf3 myDat1.dat myDat2.dat...
+"""
 
 import fileinput
 import os.path
 import sys
 import getopt
 import urllib
-
-#import xml.dom.minidom as minidom
 from lxml import etree 
 
 svninfo = """
@@ -19,7 +17,6 @@ $HeadURL: http://fgcz-svn.uzh.ch/repos/fgcz/stable/proteomics/bin/fgcz_scaffold_
 $Id: fgcz_scaffold_create_xml_driver.py 7552 2015-06-30 11:21:26Z cpanse $
 $Date: 2015-06-30 13:21:26 +0200 (Tue, 30 Jun 2015) $
 """
-
 
 scaffold_xsd = """
 <xs:schema attributeFormDefault="unqualified" elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
@@ -93,6 +90,51 @@ scaffold_xsd = """
 </xs:schema>
 """
 
+qModelStringiTRAQ_4-Plex = """
+<QuantitativeModel type="iTRAQ 4-Plex">
+                <QuantitativeSample category="Reference" description=""
+                    name="Quant 1" primary="true" reporter="iTRAQ-114"/>
+                <QuantitativeSample category="" description=""
+                    name="Quant 2" primary="false" reporter="iTRAQ-115"/>
+                <QuantitativeSample category="" description=""
+                    name="Quant 3" primary="false" reporter="iTRAQ-116"/>
+                <QuantitativeSample category="" description=""
+                    name="Quant 4" primary="false" reporter="iTRAQ-117"/>
+                <PurityCorrection>0.000,0.01000,0.925,0.0630,0.00200
+0.000,0.0200,0.919,0.0600,0.001000
+0.000,0.0300,0.920,0.0490,0.001000
+0.001000,0.0400,0.920,0.0380,0.001000</PurityCorrection>
+</QuantitativeModel>
+"""
+qModelStringiTRAQ_8-Plex = """
+<QuantitativeModel type="iTRAQ 8-Plex">
+                <QuantitativeSample category="Reference" description=""
+                    name="Quant 1" primary="true" reporter="iTRAQ-113"/>
+                <QuantitativeSample category="category 1" description=""
+                    name="Quant 2" primary="false" reporter="iTRAQ-114"/>
+                <QuantitativeSample category="category 2" description=""
+                    name="Quant 3" primary="false" reporter="iTRAQ-115"/>
+                <QuantitativeSample category="category 3" description=""
+                    name="Quant 4" primary="false" reporter="iTRAQ-116"/>
+                <QuantitativeSample category="category 4" description=""
+                    name="Quant 5" primary="false" reporter="iTRAQ-117"/>
+                <QuantitativeSample category="category 5" description=""
+                    name="Quant 6" primary="false" reporter="iTRAQ-118"/>
+                <QuantitativeSample category="category 6" description=""
+                    name="Quant 7" primary="false" reporter="iTRAQ-119"/>
+                <QuantitativeSample category="category 7" description=""
+                    name="Quant 8" primary="false" reporter="iTRAQ-121"/>
+                <PurityCorrection>0.000,0.000,0.929,0.0689,0.00220
+0.000,0.00940,0.930,0.0590,0.00160
+0.000,0.0188,0.931,0.0490,0.001000
+0.000,0.0282,0.932,0.0390,0.000700
+0.000600,0.0377,0.933,0.0288,0.000
+0.000900,0.0471,0.933,0.0188,0.000
+0.00140,0.0566,0.933,0.00870,0.000
+0.000,0.000,0.000,0.000,0.000</PurityCorrection>
+</QuantitativeModel>
+"""
+
 # MAIN
 
 def main(argv):
@@ -105,6 +147,7 @@ def main(argv):
     workunitId = None
     qModel = None
     qModelSpace = ['iTRAQ_4-Plex', 'iTRAQ_8-Plex']
+
     try:
         opts, args = getopt.getopt(argv, "m:x:q:w:g:", ["mudpit=", "xtandem=", "qmodel=", "workunit=", "gelcms="])
     except getopt.GetoptError:
@@ -220,52 +263,10 @@ def main(argv):
     map(lambda item: DisplayThresholds.set(*item), displayThresholdsDetails)
     
     if qModel == "iTRAQ_4-Plex":
-        qModelString = """
-<QuantitativeModel type="iTRAQ 4-Plex">
-                <QuantitativeSample category="Reference" description=""
-                    name="Quant 1" primary="true" reporter="iTRAQ-114"/>
-                <QuantitativeSample category="" description=""
-                    name="Quant 2" primary="false" reporter="iTRAQ-115"/>
-                <QuantitativeSample category="" description=""
-                    name="Quant 3" primary="false" reporter="iTRAQ-116"/>
-                <QuantitativeSample category="" description=""
-                    name="Quant 4" primary="false" reporter="iTRAQ-117"/>
-                <PurityCorrection>0.000,0.01000,0.925,0.0630,0.00200
-0.000,0.0200,0.919,0.0600,0.001000
-0.000,0.0300,0.920,0.0490,0.001000
-0.001000,0.0400,0.920,0.0380,0.001000</PurityCorrection>
-            </QuantitativeModel>
-            """
+        qModelString = qModelStringiTRAQ_4-Plex
         BiologicalSample.append(etree.fromstring(qModelString))
     elif qModel == "iTRAQ_8-Plex":
-        qModelString = """
-<QuantitativeModel type="iTRAQ 8-Plex">
-                <QuantitativeSample category="Reference" description=""
-                    name="Quant 1" primary="true" reporter="iTRAQ-113"/>
-                <QuantitativeSample category="category 1" description=""
-                    name="Quant 2" primary="false" reporter="iTRAQ-114"/>
-                <QuantitativeSample category="category 2" description=""
-                    name="Quant 3" primary="false" reporter="iTRAQ-115"/>
-                <QuantitativeSample category="category 3" description=""
-                    name="Quant 4" primary="false" reporter="iTRAQ-116"/>
-                <QuantitativeSample category="category 4" description=""
-                    name="Quant 5" primary="false" reporter="iTRAQ-117"/>
-                <QuantitativeSample category="category 5" description=""
-                    name="Quant 6" primary="false" reporter="iTRAQ-118"/>
-                <QuantitativeSample category="category 6" description=""
-                    name="Quant 7" primary="false" reporter="iTRAQ-119"/>
-                <QuantitativeSample category="category 7" description=""
-                    name="Quant 8" primary="false" reporter="iTRAQ-121"/>
-                <PurityCorrection>0.000,0.000,0.929,0.0689,0.00220
-0.000,0.00940,0.930,0.0590,0.00160
-0.000,0.0188,0.931,0.0490,0.001000
-0.000,0.0282,0.932,0.0390,0.000700
-0.000600,0.0377,0.933,0.0288,0.000
-0.000900,0.0471,0.933,0.0188,0.000
-0.00140,0.0566,0.933,0.00870,0.000
-0.000,0.000,0.000,0.000,0.000</PurityCorrection>
-            </QuantitativeModel>
-"""
+        qModelString = qModelStringiTRAQ_8-Plex
         BiologicalSample.append(etree.fromstring(qModelString))
     else:
         pass
@@ -283,7 +284,6 @@ def main(argv):
 
     rough_string = etree.tostring(root, pretty_print=True)
 
-    # TODO(cp): xsd check would be nice to have
     try:
         schema = etree.XMLSchema(etree.XML(scaffold_xsd))
     except:
