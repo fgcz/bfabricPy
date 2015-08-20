ab1ad846cee67fb9dd84983f933f703a0223be44
cpanse <cp@fgcz.ethz.ch>
2015-08-20 16:38:06 +0200

    added workder

--- apps/maxquant/rpc-maxquant-server.py
+++ apps/maxquant/rpc-maxquant-server.py
@@ -24,6 +24,29 @@ import hashlib
 import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+def myExecWorker0(cmdLine):
+    myPid = None
+    rcode = None
+
+    print "exec|cmd='{0}'".format(cmdLine)
+    try:
+        tStart = time.time()
+        p = subprocess.Popen(cmdLine, shell=True)
+
+        myPid = p.pid
+        rcode = p.wait()
+
+        tStop = time.time()
+
+        p.terminate()
+
+    except OSError as e:
+        print "exception|pid={0}|OSError={1}".format(myPid, e)
+
+    print "completed|pid={0}|time={1}|cmd='{2}'".format(myPid, tStop - tStart, cmdLine)
+
+    return rcode
+
 
 def maxquant(config=None):
     return "HELLO FROM RPC SERVER")
@@ -34,5 +57,5 @@ if __name__ == "__main__":
     port = 9999
     server = SimpleXMLRPCServer(("130.60.81.74", port))
     print "Listening on port {0} ...".format(port)
-    server.register_function(maxquant, "maxquant")
+    server.register_function(myExecWorker0(, "worker")
     server.serve_forever()
