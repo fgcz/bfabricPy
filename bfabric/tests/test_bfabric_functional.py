1618bc6142fed1cd1f11d6b9273115876f8f63d5
Christian Panse <cp@fgcz.ethz.ch>
2020-06-11 11:29:16 +0200

    fixed type

--- bfabric/tests/test_bfabric_functional.py
+++ bfabric/tests/test_bfabric_functional.py
@@ -45,7 +45,7 @@ class BfabricFunctionalTestCase(unittest.TestCase):
 
 
         workunit = B.save_object("workunit",
-            obj={"name": "unit test", "status": "pending", 'containerid': 3061, 'applicationid': applicationaid, 'inputdatasetid': 32428})
+            obj={"name": "unit test", "status": "pending", 'containerid': 3061, 'applicationid': applicationid, 'inputdatasetid': 32428})
 
         workunitid = int(workunit[0]._id)
         msg = "workunitid should be a positig integer."
