a92ccfdfd39aa5f680ca0fba3435f9a2b0a0e7c2
Witold Wolski <wewolski@gmail.com>
2020-10-07 15:06:31 +0200

    closes #35

--- bfabric/scripts/bfabric_save_fasta.py
+++ bfabric/scripts/bfabric_save_fasta.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import yaml
-import xmlrpclib
+#import xmlrpclib
 import hashlib
 from optparse import OptionParser
 from bfabric import Bfabric
@@ -12,28 +12,28 @@ FASTAHTTPROOT="/fasta/"
 BFABRICSTORAGEID = 2
 BFABRICAPPLIATIONID = 61
 
-def save_fasta(projectid=1875, fasta_file="p1875_db10_20170817.fasta"):
+def save_fasta(containerid=1875, fasta_file="p1875_db10_20170817.fasta"):
     bfapp = Bfabric()
 
     try:
-        print "reading stdin"
+        print("reading stdin")
         description = sys.stdin.read()
     except:
-        print "reading from stdin failed."
+        print("reading from stdin failed.")
         raise
 
     try:
         md5 = hashlib.md5(open(fasta_file, 'rb').read()).hexdigest()
     except:
-        print "computing file checksum failed."
+        print("computing file checksum failed.")
         raise
 
     resource = bfapp.read_object(endpoint='resource', obj={'filechecksum': md5})
 
     try:    
-        print "resource(s) already exist.".format(resource[0]._id)
+        print("resource(s) already exist.".format(resource[0]._id))
         resource = bfapp.save_object(endpoint='resource', obj={'id': resource[0]._id, 'description': description})
-        print resource
+        print(resource)
         return
     except:
         pass
@@ -42,7 +42,7 @@ def save_fasta(projectid=1875, fasta_file="p1875_db10_20170817.fasta"):
     try:
         workunit = bfapp.save_object(endpoint='workunit',
                                  obj={'name': "FASTA: {}".format(os.path.basename(fasta_file)),
-                                      'projectid': projectid,
+                                      'containerid': containerid,
                                       'applicationid': BFABRICAPPLIATIONID})
         print (workunit)
     except:
@@ -61,14 +61,14 @@ def save_fasta(projectid=1875, fasta_file="p1875_db10_20170817.fasta"):
 
 
     resource = bfapp.save_object(endpoint='resource', obj=obj)
-    print resource
+    print(resource)
 
     workunit = bfapp.save_object(endpoint='workunit',
                                  obj={'id': workunit[0]._id, 'status': 'available'})
     print (workunit)
 
 if __name__ == "__main__":
-    save_fasta(projectid=sys.argv[1], fasta_file=sys.argv[2])
+    save_fasta(containerid=sys.argv[1], fasta_file=sys.argv[2])
 
 
     #p#rint (workunit)
