4635327326f9cab5993ac5f1ca1761f70ba8ccef
Christian Panse <cp@fgcz.ethz.ch>
2021-10-14 08:54:38 +0200

    Enable endpoint barcode #40

--- bfabric/__init__.py
+++ bfabric/__init__.py
@@ -7,8 +7,8 @@ msg = "\033[93m{} version {} (2021-09-07) -- \"{}\"\
     \nCopyright (C) 2019 Functional Genomics Center Zurich\033[0m\n\n"\
     .format(name, __version__, alias)
 
-endpoints = sorted(['access', 'annotation', 'application',
-        'attachement', 'comment', 'container', 'dataset', 'executable',
+endpoints = sorted(['access', 'annotation', 'application', 'attachement',
+        'barcodes', 'comment', 'container', 'dataset', 'executable',
         'externaljob', 'groupingvar', 'importresource', 'mail',
         'parameter', 'plate', 'project', 'resource', 'sample',
         'workflow', 'workflowstep',
