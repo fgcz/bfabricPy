b6d2b783f7bac287ba8b3b2424b8592864034183
Christian Panse <cp@fgcz.ethz.ch>
2023-02-07 11:13:41 +0100

    Create class for traversal of the TMT sample graph
    - add class
    - some renaming
    - add annotation dict
    - add manifest dict
    - check if sample contains a multiplex tag
    - collect for populating the result tables
    (fragpipe manifest file and annotation.txt files)

--- bfabric/scripts/bfabric_sample_graph_traversal.py
+++ bfabric/scripts/bfabric_sample_graph_traversal.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: latin1 -*-
 
 """
@@ -6,6 +6,9 @@ travers the bfabric sample graph
 input: resource id (sample id)
 
 output: FragPipe required TMT settings
+
+usage:
+   bfabric_sample_graph_traversal.py 449326  
 """
 
 import sys
@@ -14,29 +17,83 @@ import bfabric
 from random import randint
 from time import sleep
 
-B = bfabric.Bfabric()
 
-L = []
-VISITED = []
+class SampleGraph:
+    B = bfabric.Bfabric()
 
-def __sample_dfs (sampleid):
-   res = B.read_object(endpoint='sample', obj={'id': sampleid} , page=1)
+    # use as stack for the DFS
+    L = []
 
-   if 'parent' in res[0]:
-       for s in res[0].parent:
-           if not s._id in VISITED:
-               print("\t{} -> {}".format(sampleid, s._id))
-               VISITED.append(s._id)
-               L.append(s._id)
+    # keeps the visited sample ids
+    VISITED = []
 
-   print("DEBUG = {}".format(len(L)))
-   while (len(L) > 0):
-       u = L[0]
-       L.remove(u)
-       __sample_dfs(u)
+    # fragpipe-manifest
+    # key is resource id and the value is a list experiments
+    manifest = {}
+
+    # annimation.txt
+    # data structure for keeping annotation.txt infors (de-multiplexed data) containing the tagging
+    annotation = {}
+
+    def init(self):
+        pass
+
+    def traverse(self, childSampleId):
+        """
+        fill up the internal data structure
+        for producing the manifest and annotation.txt files for each exp.
+
+
+        TODO: when to we populate the table? now or later?
+        """
+        res = self.B.read_object(endpoint='sample', obj={'id': childSampleId}, page=1)
+        childSample = res[0]
+
+        if "multiplexid" in  childSample:
+            # in this special case we reached last level keeping the tag
+            print ("\t# {} {}".format(childSample._id, childSample.multiplexid))
+
+        if 'parent' in childSample:
+            self.annotation[childSampleId] = [x._id for x in childSample.parent]
+            for parent in res[0].parent:
+                if not parent._id in self.VISITED:
+                    print("\t{} -> {}".format(parent._id, childSampleId))
+                    self.VISITED.append(parent._id)
+                    self.L.append(parent._id)
+
+        print("\t# DEBUG = {}".format(len(self.L)))
+
+        while (len(self.L) > 0):
+            u = self.L[0]
+            self.L.remove(u)
+            self.traverse(u)
+
+    def run(self, inputResource):
+        for resourceId in inputResource:
+            # sampleId = getSampleOfResource(resourceId)
+            # traverse(sampleId)
+            pass
+
+    def writetable(self, childSampleID):
+        MSsample = self.annotation[childSampleID][0]
+        print(childSampleID, MSsample)
+        print(MSsample, self.annotation[MSsample])
+        print (self.annotation)
 
         
 if __name__ == "__main__":
+
+    # TODO(cp): read WU12345.yaml file
+
+    # constructor
+    G = SampleGraph()
+
+    # TODO(cp): G.run(listOfResources)
     if len(sys.argv) > 1:
-        __sample_dfs(int(sys.argv[1]))
+        G.traverse(int(sys.argv[1]))
+
+
+    G.writetable(int(sys.argv[1]))
+
+
 
