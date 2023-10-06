d77fafc961837d61516177cb1b39975c3e10bea1
Christian Panse <cp@fgcz.ethz.ch>
2023-10-06 11:05:41 +0200

    Version inc

--- bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
+++ bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: latin1 -*-
-
 """
 Copyright (C) 2023 Functional Genomics Center Zurich ETHZ|UZH. All rights reserved.
 
@@ -9,7 +8,7 @@ Lists applications that are not available on bfabric.
 Author:
  Christian Panse <cp@fgcz.ethz.ch>
 
-Licensed under  GPL version 3
+Licensed under GPL version 3
 """
 
 import sys
@@ -28,21 +27,18 @@ def render_output(wu):
         "PROCESSING": "\033[34mProcessing\033[0m",
         "FAILED"    : "\033[31mFailed    \033[0m"}
 
-
     for x in wu:
         if x.status in cm:
             statuscol = cm[x.status]
         else:
             statuscol = "\033[36m{} \033[0m".format(x.status)
-
-
-        print("A{applicationid:3} WU{wuid} {cdate} {status} {createdby:12} {name}"
+        print("A{aid:3} WU{wuid} {cdate} {status} {createdby:12} {name}"
                .format(status = statuscol,
                     cdate = x.created,
                     wuid = x._id,
                     createdby = x.createdby,
                     name = x.name,
-                    applicationid = x.application._id))
+                    aid = x.application._id))
 
 if __name__ == "__main__":
     B = bfabric.Bfabric()
