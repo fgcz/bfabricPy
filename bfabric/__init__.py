dba80001317d42972ddc1e95fd040526757747c2
Christian Panse <cp@fgcz.ethz.ch>
2023-11-03 12:14:17 +0100

    merge

--- bfabric/__init__.py
+++ bfabric/__init__.py
@@ -3,21 +3,43 @@ from ._version import __version__
 name = "bfabricPy"
 alias = "suds-py3"
 
-msg = "\033[93m{} version {} (2023-10-11) -- \"{}\"\
+msg = "\033[93m{} version {} (2023-11-03) -- \"{}\"\
     \nCopyright (C) 2014-2023 Functional Genomics Center Zurich\033[0m\n\n"\
     .format(name, __version__, alias)
 
-endpoints = sorted(['access', 'annotation', 'application', 'attachement',
-        'barcodes', 'comment', 'container', 'dataset', 'executable',
-        'externaljob', 'groupingvar', 'importresource', 'mail',
-        'parameter', 'plate', 'project', 'resource', 'sample',
-        'workflow', 'workflowstep',
-        'storage', 'user', 'workunit', 'charge', 'order', 'instrument'])
+endpoints = sorted([
+        'annotation',
+        'application',
+        'attachement',
+        'barcodes',
+        'charge',
+        'comment',
+        'container',
+        'dataset',
+        'executable',
+        'externaljob',
+        'groupingvar',
+        'importresource',
+        'instrument',
+        'instrumentevent',
+        'mail',
+        'order',
+        'parameter',
+        'plate',
+        'project',
+        'resource',
+        'sample',
+        'storage',
+        'user',
+        'workflow',
+        'workflowstep',
+        'workunit'
+])
 
 # for unit tests
-project=403
-container=project
-application=217
+project = 403
+container = project
+application = 217
 
 from bfabric.bfabric import Bfabric
 from bfabric.bfabric import BfabricWrapperCreator
