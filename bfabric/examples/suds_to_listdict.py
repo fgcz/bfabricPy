0af5a9526e2913f8306fd31b531340f252c4509a
Aleksejs Fomins <29147544+aleksejs-fomins@users.noreply.github.com>
2024-04-09 15:23:38 +0200

    Update bfabric/examples/suds_to_listdict.py
    Co-authored-by: Leonardo Schwarz <leoschwarz@users.noreply.github.com>

--- bfabric/examples/suds_to_listdict.py
+++ bfabric/examples/suds_to_listdict.py
@@ -5,8 +5,8 @@ from typing import List
 
 def read_shallow(b: bfabric.Bfabric, endpoint: str, obj: dict) -> List[dict]:
     response = b.read_object(endpoint=endpoint, obj=obj, plain=True)
-    responseDict = suds_to_json(response)
-    return responseDict[endpoint]
+    response_dict = suds_to_json(response)
+    return response_dict[endpoint]
 
 b = bfabric.Bfabric()
 resLstDict = read_shallow(b, 'user', {'login': 'fomins'})
