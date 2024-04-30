4774ee6e679616f2cb5819ad6cb3e538712b7c75
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-04-30 12:13:48 +0200

    update another script for illustrative purposes

--- bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
+++ bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
@@ -12,46 +12,53 @@ Licensed under GPL version 3
 """
 
 import sys
-import bfabric
-
 from datetime import datetime, timedelta
+
+from bfabric.bfabric2 import default_client
+
 
 def print_color_msg(msg, color="93"):
     msg = "\033[{color}m--- {} ---\033[0m\n".format(msg, color=color)
     sys.stderr.write(msg)
 
-def render_output(wu):
-    wu = list(filter(lambda x: x.createdby not in  ["gfeeder", "itfeeder"], wu))
 
-    cm = {"PENDING" : "\033[33mPending   \033[0m",
+def render_output(wu):
+    wu = list(filter(lambda x: x["createdby"] not in ["gfeeder", "itfeeder"], wu))
+
+    cm = {
+        "PENDING": "\033[33mPending   \033[0m",
         "PROCESSING": "\033[34mProcessing\033[0m",
-        "FAILED"    : "\033[31mFailed    \033[0m"}
+        "FAILED": "\033[31mFailed    \033[0m",
+    }
 
     for x in wu:
-        if x.status in cm:
-            statuscol = cm[x.status]
+        if x["status"] in cm:
+            statuscol = cm[x["status"]]
         else:
-            statuscol = "\033[36m{} \033[0m".format(x.status)
-        print("A{aid:3} WU{wuid} {cdate} {status} {createdby:12} {name}"
-               .format(status = statuscol,
-                    cdate = x.created,
-                    wuid = x._id,
-                    createdby = x.createdby,
-                    name = x.name,
-                    aid = x.application._id))
+            statuscol = "\033[36m{} \033[0m".format(x["status"])
+        print(
+            "A{aid:3} WU{wuid} {cdate} {status} {createdby:12} {name}".format(
+                status=statuscol,
+                cdate=x["created"],
+                wuid=x["id"],
+                createdby=x["createdby"],
+                name=x["name"],
+                aid=x["application"]["id"]
+            )
+        )
 
 if __name__ == "__main__":
-    B = bfabric.Bfabric()
-    d = datetime.today() - timedelta(days=14)
+    client = default_client()
+    date_cutoff = datetime.today() - timedelta(days=14)
 
-    print_color_msg("list not available proteomics workunits created after {}".format(d))
+    print_color_msg(
+        "list not available proteomics workunits created after {}".format(date_cutoff)
+    )
 
-    for status in ['Pending', 'Processing', 'Failed']:
-        pwu = B.read_object(endpoint = 'workunit',
-                            obj = {'status': status, 'createdafter': d},
-                            plain = True,
-                            page = 1)
-        try:
-            render_output(pwu.workunit)
-        except:
-            pass
+    for status in ["Pending", "Processing", "Failed"]:
+        pending_workunits = client.read(
+            endpoint="workunit",
+            obj={"status": status, "createdafter": date_cutoff},
+        ).to_list_dict()
+
+        render_output(pending_workunits)
