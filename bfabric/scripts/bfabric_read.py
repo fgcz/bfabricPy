4f7d9dd68f6e1744d96447a91fe9b75d237ec238
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-04-30 16:09:35 +0200

    version using typer

--- bfabric/scripts/bfabric_read.py
+++ bfabric/scripts/bfabric_read.py
@@ -13,11 +13,13 @@ License:
 See also:
     http://fgcz-bfabric.uzh.ch/bfabric/executable?wsdl
 """
-import argparse
+import enum
 import json
 import sys
 import time
-from typing import Optional
+from typing import Optional, Annotated
+
+import typer
 
 import bfabric
 from bfabric.bfabric2 import default_client
@@ -27,9 +29,21 @@ def print_color_msg(msg, color="93"):
     print(f"\033[{color}m--- {msg} ---\033[0m", file=sys.stderr)
 
 
+class OutputFormat(enum.Enum):
+    auto = "auto"
+    json = "json"
+    table_tsv = "table_tsv"
+
+
 def bfabric_read(
-    endpoint: str, attribute: Optional[str], value: Optional[str], output_format: str
+    endpoint: str,
+    attribute: Annotated[Optional[str], typer.Argument()] = None,
+    value: Annotated[Optional[str], typer.Argument()] = None,
+    format: OutputFormat = "auto",
 ):
+    if endpoint not in bfabric.endpoints:
+        raise ValueError(f"endpoint: invalid choice {endpoint}")
+
     query_obj = {attribute: value} if value is not None else {}
     print_color_msg(f"query = {query_obj}")
 
@@ -44,12 +58,12 @@ def bfabric_read(
         possible_attributes = set(res[0].keys())
         print_color_msg(f"possible attributes are: {possible_attributes}")
 
-    if output_format == "auto":
-        output_format = "json" if len(res) < 2 else "table_tsv"
+    if format == OutputFormat.auto:
+        format = OutputFormat.json if len(res) < 2 else OutputFormat.table_tsv
 
-    if output_format == "json":
+    if format == OutputFormat.json:
         print(json.dumps(res, indent=2))
-    elif output_format == "table_tsv":
+    elif format == OutputFormat.table_tsv:
         for x in res:
             try:
                 print(
@@ -57,25 +71,15 @@ def bfabric_read(
                 )
             except (KeyError, TypeError):
                 print(f'{x["id"]}\t{x["createdby"]}\t{x["modified"]}')
+    else:
+        raise ValueError(f"unknown output format {format}")
 
     print_color_msg(f"number of query result items = {len(res)}")
     print_color_msg(f"query time = {round(end_time - start_time, 2)} seconds")
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--format",
-        help="output format",
-        choices=["json", "table_tsv", "auto"],
-        default="auto",
-        dest="output_format",
-    )
-    parser.add_argument("endpoint", help="endpoint to query", choices=bfabric.endpoints)
-    parser.add_argument("attribute", help="attribute to query for", nargs="?")
-    parser.add_argument("value", help="value to query for", nargs="?")
-    args = parser.parse_args()
-    bfabric_read(**vars(args))
+    typer.run(bfabric_read)
 
 
 if __name__ == "__main__":
