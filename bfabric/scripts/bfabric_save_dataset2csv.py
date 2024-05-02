d2953786cdefd9f8ec6f175655388c02125d15b8
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-02 16:35:15 +0200

    increase quality

--- bfabric/scripts/bfabric_save_dataset2csv.py
+++ bfabric/scripts/bfabric_save_dataset2csv.py
@@ -16,40 +16,48 @@ Example: bfabric_save_dataset2csv.py --id 32335 && cat dataset.csv
 """
 
 import argparse
+from pathlib import Path
 
 from bfabric.bfabric2 import Bfabric, get_system_auth
 
 
-def dataset2csv(ds, outputfile, sep=","):
-    # ds.attribute contains the list of columns name
-    with open(outputfile, "w") as f:
-        f.write("{}\n".format(sep.join(map(lambda x: x["name"], ds["attribute"]))))
-        for i in ds["item"]:
+def dataset2csv(dataset: dict, output_path: Path, sep: str = ",") -> None:
+    """Writes the `dataset` content to csv file at `output_path`."""
+    # dataset["attribute"] contains the list of columns name
+    with output_path.open("w") as f:
+        f.write("{}\n".format(sep.join(map(lambda x: x["name"], dataset["attribute"]))))
+        for i in dataset["item"]:
             # sort values based on the columns order in attributeposition
             fields = [(x.get("value") or "", x["attributeposition"]) for x in i["field"]]
             fields.sort(key=lambda y: int(y[1]))
-            f.write("{}\n".format(sep.join([t[0] for t in fields])))
+            print(sep.join([t[0] for t in fields]), file=f)
 
 
-def bfabric_save_dataset2csv(dataset_id: int, out_dir: str):
-    bfapp = Bfabric(*get_system_auth())
-    results = bfapp.read(endpoint="dataset", obj={"id": dataset_id}).to_list_dict()
+def bfabric_save_dataset2csv(dataset_id: int, out_dir: Path) -> None:
+    """Saves the dataset with id `dataset_id` to a csv file at `out_dir`."""
+    client = Bfabric(*get_system_auth())
+    results = client.read(endpoint="dataset", obj={"id": dataset_id}).to_list_dict()
     if not results:
-        raise RuntimeError("No dataset found with id '{}'".format(dataset_id))
+        raise RuntimeError(f"No dataset found with id '{dataset_id}'")
     dataset = results[0]
-
+    output_path = out_dir / "dataset.csv"
     try:
-        dataset2csv(dataset, "{}/dataset.csv".format(out_dir))
+        dataset2csv(dataset, output_path=output_path)
     except Exception:
-        print("The writing process to '{}'/dataset.csv failed.".format(out_dir))
+        print(f"The writing process to '{output_path}' failed.")
         raise
 
 
-def main():
+def main() -> None:
+    """Parses arguments and calls `bfabric_save_dataset2csv`."""
     parser = argparse.ArgumentParser(description="Save a B-Fabric dataset to a csv file")
     parser.add_argument("--id", metavar="int", required=True, help="dataset id", type=int)
     parser.add_argument(
-        "--dir", required=False, default="./", help="the path to the directory where to save the csv file"
+        "--dir",
+        required=False,
+        type=Path,
+        default=".",
+        help="the path to the directory where to save the csv file",
     )
     args = parser.parse_args()
     bfabric_save_dataset2csv(out_dir=args.dir, dataset_id=args.id)
