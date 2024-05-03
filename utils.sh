83f22f9db33ca747f3a90bc4f872f52d6d228a50
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-03 14:43:13 +0200

    clean

---
+++ utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clean_script() {
+  target_name=$1
+  target_path=$(find . -name $target_name | head -n 1)
+  if [ -z $target_path ]; then
+    echo "No $target_name script found"
+  else
+    echo "Cleaning $target_path"
+
+    set -x
+    isort "$target_path"
+    black "$target_path"
+    ruff check "$target_path"
+    set +x
+  fi
+}
