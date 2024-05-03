2c10a8dffff190988c2a029339dd441765f88ed3
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-03 11:50:37 +0200

    clean

--- bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
+++ bfabric/scripts/bfabric_list_not_available_proteomics_workunits.py
@@ -3,7 +3,7 @@
 """
 Copyright (C) 2023 Functional Genomics Center Zurich ETHZ|UZH. All rights reserved.
 
-Lists applications that are not available on bfabric.
+Lists proteomics workunits that are not available on bfabric.
 
 Author:
  Christian Panse <cp@fgcz.ethz.ch>
@@ -11,58 +11,69 @@ Author:
 Licensed under GPL version 3
 """
 
-import sys
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
+
+from rich.console import Console
+from rich.table import Table
 
 from bfabric.bfabric2 import default_client
 
 
-def print_color_msg(msg, color="93"):
-    msg = "\033[{color}m--- {} ---\033[0m\n".format(msg, color=color)
-    sys.stderr.write(msg)
+def render_output(workunits_by_status):
+    """Renders the output as a table."""
+    table = Table()
+    table.add_column("AID", no_wrap=True)
+    table.add_column("WUID", no_wrap=True)
+    table.add_column("Created", no_wrap=True)
+    table.add_column("Status", no_wrap=True)
+    table.add_column("Created by", no_wrap=True, max_width=12)
+    table.add_column("Name", no_wrap=False)
 
+    for status, workunits in workunits_by_status.items():
+        workunits = [x for x in workunits if x["createdby"] not in ["gfeeder", "itfeeder"]]
+        status_color = {
+            "Pending": "yellow",
+            "Processing": "blue",
+            "Failed": "red",
+        }.get(status, "black")
 
-def render_output(workunits):
-    workunits = [x for x in workunits if x["createdby"] not in ["gfeeder", "itfeeder"]]
-    color_map = {
-        "PENDING": "\033[33mPending   \033[0m",
-        "PROCESSING": "\033[34mProcessing\033[0m",
-        "FAILED": "\033[31mFailed    \033[0m",
-    }
-
-    for x in workunits:
-        statuscol = color_map.get(x["status"], f"\033[36m{x['status']} \033[0m")
-        print(
-            "A{aid:3} WU{wuid} {cdate} {status} {createdby:12} {name}".format(
-                status=statuscol,
-                cdate=x["created"],
-                wuid=x["id"],
-                createdby=x["createdby"],
-                name=x["name"],
-                aid=x["application"]["id"],
+        for x in workunits:
+            app_url = f"https://fgcz-bfabric.uzh.ch/bfabric/application/show.html?id={x['application']['id']}"
+            wu_url = f"https://fgcz-bfabric.uzh.ch/bfabric/workunit/show.html?id={x['id']}&tab=details"
+            table.add_row(
+                f"[link={app_url}]A{x['application']['id']:3}[/link]",
+                f"[link={wu_url}]WU{x['id']}[/link]",
+                x["created"],
+                f"[{status_color}]{status}[/{status_color}]",
+                x["createdby"],
+                x["name"],
             )
-        )
+
+    console = Console()
+    console.print(table)
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("--max-age", type=int, help="Max age of workunits in days", default=14)
+    """Lists proteomics work units that are not available on bfabric."""
+    parser = ArgumentParser(description="Lists proteomics work units that are not available on bfabric.")
+    parser.add_argument("--max-age", type=int, help="Max age of work units in days", default=14)
     args = parser.parse_args()
     client = default_client()
     date_cutoff = datetime.today() - timedelta(days=args.max_age)
 
-    print_color_msg(
-        "list not available proteomics workunits created after {}".format(date_cutoff)
+    Console(stderr=True).print(
+        f"--- list not available proteomics work units created after {date_cutoff}---", style="bright_yellow"
     )
 
+    workunits_by_status = {}
     for status in ["Pending", "Processing", "Failed"]:
-        pending_workunits = client.read(
+        workunits_by_status[status] = client.read(
             endpoint="workunit",
             obj={"status": status, "createdafter": date_cutoff},
         ).to_list_dict()
 
-        render_output(pending_workunits)
+    render_output(workunits_by_status)
 
 
 if __name__ == "__main__":
