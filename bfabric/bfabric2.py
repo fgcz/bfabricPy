59c1724b371fc325873f134af00c2f89c423ba5b
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2024-05-08 17:06:04 +0200

    Refactor upload scripts (#92)
    Refactors bfabric/scripts/bfabric_upload_resource.py and bfabric/scripts/bfabric_upload_submitter_executable.py, however the latter was not tested.

--- bfabric/bfabric2.py
+++ bfabric/bfabric2.py
@@ -299,6 +299,28 @@ class Bfabric:
             result.assert_success()
         return result
 
+    def upload_resource(
+        self, resource_name: str, content: bytes, workunit_id: int, check: bool = True
+    ) -> ResultContainer:
+        """Uploads a resource to B-Fabric, only intended for relatively small files that will be tracked by B-Fabric
+        and not one of the dedicated experimental data stores.
+        :param resource_name: the name of the resource to create (the same name can only exist once per workunit)
+        :param content: the content of the resource as bytes
+        :param workunit_id: the workunit ID to which the resource belongs
+        :param check: whether to check for errors in the response
+        """
+        content_encoded = base64.b64encode(content).decode()
+        return self.save(
+            endpoint="resource",
+            obj={
+                "base64": content_encoded,
+                "name": resource_name,
+                "description": "base64 encoded file",
+                "workunitid": workunit_id,
+            },
+            check=check,
+        )
+
     def _read_page(self, readid: bool, endpoint: str, query: dict[str, Any], idonly: bool = False, page: int = 1):
         """Reads the specified page of objects from the specified endpoint that match the query."""
         if readid:
