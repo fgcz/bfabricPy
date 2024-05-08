94d8d4f38d1bf6c20f016a9f851ea0b16e728784
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-08 16:12:34 +0200

    refactor

--- bfabric/scripts/bfabric_read.py
+++ bfabric/scripts/bfabric_read.py
@@ -13,25 +13,28 @@ License:
 See also:
     http://fgcz-bfabric.uzh.ch/bfabric/executable?wsdl
 """
+from __future__ import annotations
 import argparse
 import json
 import time
 import yaml
-from typing import Optional
+from typing import Optional, Any
 
 from rich.console import Console
 from rich.table import Table
 
 import bfabric
+from bfabric import BfabricConfig
 from bfabric.bfabric2 import Bfabric
 
 
-def bfabric_read(client: Bfabric, endpoint: str, attribute: Optional[str], value: Optional[str], output_format: str) -> None:
+def bfabric_read(
+    client: Bfabric, endpoint: str, attribute: Optional[str], value: Optional[str], output_format: str
+) -> None:
     """Reads one or several items from a B-Fabric endpoint and prints them."""
     if attribute is not None and value is None:
         message = "value must be provided if attribute is provided"
         raise ValueError(message)
-
 
     query_obj = {attribute: value} if value is not None else {}
     console_info = Console(style="bright_yellow", stderr=True)
@@ -42,18 +45,11 @@ def bfabric_read(client: Bfabric, endpoint: str, attribute: Optional[str], value
     results = client.read(endpoint=endpoint, obj=query_obj)
     end_time = time.time()
     res = sorted(results.to_list_dict(drop_empty=False), key=lambda x: x["id"])
-
     if res:
         possible_attributes = sorted(set(res[0].keys()))
         console_info.print(f"--- possible attributes = {possible_attributes} ---")
 
-    if output_format == "auto":
-        if len(res) < 2:
-            output_format = "json"
-        elif console_out.is_interactive:
-            output_format = "table_rich"
-        else:
-            output_format = "table_tsv"
+    output_format = _determine_output_format(console_out=console_out, output_format=output_format, n_results=len(res))
     console_info.print(f"--- output format = {output_format} ---")
 
     if output_format == "json":
@@ -61,28 +57,47 @@ def bfabric_read(client: Bfabric, endpoint: str, attribute: Optional[str], value
     elif output_format == "yaml":
         print(yaml.dump(res))
     elif output_format == "table_tsv":
-        for x in res:
-            try:
-                print(f'{x["id"]}\t{x["createdby"]}\t{x["modified"]}\t{x["name"]}\t{x["groupingvar"]["name"]}')
-            except (KeyError, TypeError):
-                print(f'{x["id"]}\t{x["createdby"]}\t{x["modified"]}')
+        _print_table_tsv(res)
     elif output_format == "table_rich":
-        table = Table("Id", "Created By", "Modified", "Name", "Grouping Var")
-        for x in res:
-            entry_url = f"{client.config.base_url}/{endpoint}/show.html?id={x['id']}"
-            table.add_row(
-                f"[link={entry_url}]{x['id']}[/link]",
-                str(x["createdby"]),
-                str(x["modified"]),
-                str(x["name"]),
-                str(x.get("groupingvar", {}).get("name", "")),
-            )
-        console_out.print(table)
+        _print_table_rich(client.config, console_out, endpoint, res)
     else:
         raise ValueError(f"output format {output_format} not supported")
 
     console_info.print(f"--- number of query result items = {len(res)} ---")
     console_info.print(f"--- query time = {end_time - start_time:.2f} seconds ---")
+
+
+def _print_table_rich(config: BfabricConfig, console_out: Console, endpoint: str, res: list[dict[str, Any]]) -> None:
+    table = Table("Id", "Created By", "Modified", "Name", "Grouping Var")
+    for x in res:
+        entry_url = f"{config.base_url}/{endpoint}/show.html?id={x['id']}"
+        table.add_row(
+            f"[link={entry_url}]{x['id']}[/link]",
+            str(x["createdby"]),
+            str(x["modified"]),
+            str(x["name"]),
+            str(x.get("groupingvar", {}).get("name", "")),
+        )
+    console_out.print(table)
+
+
+def _print_table_tsv(res) -> None:
+    for x in res:
+        try:
+            print(f'{x["id"]}\t{x["createdby"]}\t{x["modified"]}\t{x["name"]}\t{x["groupingvar"]["name"]}')
+        except (KeyError, TypeError):
+            print(f'{x["id"]}\t{x["createdby"]}\t{x["modified"]}')
+
+
+def _determine_output_format(console_out: Console, output_format: str, n_results: int) -> str:
+    if output_format == "auto":
+        if n_results < 2:
+            output_format = "json"
+        elif console_out.is_interactive:
+            output_format = "table_rich"
+        else:
+            output_format = "table_tsv"
+    return output_format
 
 
 def main() -> None:
