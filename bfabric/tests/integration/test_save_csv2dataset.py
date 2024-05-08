9c2bff9aab2e1680f93df6a03f66924c313ff764
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-08 15:08:42 +0200

    update

--- bfabric/tests/integration/test_save_csv2dataset.py
+++ bfabric/tests/integration/test_save_csv2dataset.py
@@ -8,21 +8,7 @@ from tempfile import TemporaryDirectory
 import polars as pl
 from bfabric.bfabric2 import Bfabric
 from bfabric.scripts.bfabric_save_csv2dataset import bfabric_save_csv2dataset
-
-
-class DeleteEntities:
-    def __init__(self, client: Bfabric, created_entities: list[tuple[str, int]]):
-        self.client = client
-        self.created_entities = created_entities
-
-    def __call__(self):
-        errors = []
-        for entity_type, entity_id in self.created_entities:
-            errors += self.client.delete(entity_type, entity_id, check=False).errors
-        if errors:
-            print("Error deleting entities:", errors)
-        else:
-            print("Successfully deleted entities:", self.created_entities)
+from bfabric.tests.integration.integration_test_helper import DeleteEntities
 
 
 class TestSaveCsv2Dataset(unittest.TestCase):
