b6aff02da9a15f2be53053636f07e335f7b43dc9
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-08 12:13:13 +0200

    handle commas correctly

--- bfabric/scripts/bfabric_save_dataset2csv.py
+++ bfabric/scripts/bfabric_save_dataset2csv.py
@@ -18,31 +18,31 @@ Example: bfabric_save_dataset2csv.py --id 32335 && cat dataset.csv
 import argparse
 from pathlib import Path
 
+import pandas as pd
+
 from bfabric.bfabric2 import Bfabric
 
 
-def dataset2csv(dataset: dict, output_path: Path, sep: str = ",") -> None:
+def dataset2csv(dataset: dict, output_path: Path, sep: str) -> None:
     """Writes the `dataset` content to csv file at `output_path`."""
-    # dataset["attribute"] contains the list of columns name
-    with output_path.open("w") as f:
-        f.write("{}\n".format(sep.join(map(lambda x: x["name"], dataset["attribute"]))))
-        for i in dataset["item"]:
-            # sort values based on the columns order in attributeposition
-            fields = [(x.get("value") or "", x["attributeposition"]) for x in i["field"]]
-            fields.sort(key=lambda y: int(y[1]))
-            print(sep.join([t[0] for t in fields]), file=f)
+    column_names = [x["name"] for x in dataset["attribute"]]
+    data = []
+    for item in dataset["item"]:
+        row_values = [x.get("value") for x in item["field"]]
+        data.append(dict(zip(column_names, row_values)))
+    df = pd.DataFrame(data)
+    df.to_csv(output_path, index=False, sep=sep)
 
 
-def bfabric_save_dataset2csv(dataset_id: int, out_dir: Path) -> None:
+def bfabric_save_dataset2csv(client: Bfabric, dataset_id: int, out_dir: Path, sep: str) -> None:
     """Saves the dataset with id `dataset_id` to a csv file at `out_dir`."""
-    client = Bfabric.from_config(verbose=True)
     results = client.read(endpoint="dataset", obj={"id": dataset_id}).to_list_dict()
     if not results:
         raise RuntimeError(f"No dataset found with id '{dataset_id}'")
     dataset = results[0]
     output_path = out_dir / "dataset.csv"
     try:
-        dataset2csv(dataset, output_path=output_path)
+        dataset2csv(dataset, output_path=output_path, sep=sep)
     except Exception:
         print(f"The writing process to '{output_path}' failed.")
         raise
@@ -50,6 +50,7 @@ def bfabric_save_dataset2csv(dataset_id: int, out_dir: Path) -> None:
 
 def main() -> None:
     """Parses arguments and calls `bfabric_save_dataset2csv`."""
+    client = Bfabric.from_config(verbose=True)
     parser = argparse.ArgumentParser(description="Save a B-Fabric dataset to a csv file")
     parser.add_argument("--id", metavar="int", required=True, help="dataset id", type=int)
     parser.add_argument(
@@ -59,8 +60,9 @@ def main() -> None:
         default=".",
         help="the path to the directory where to save the csv file",
     )
+    parser.add_argument("--sep", default=",", help="the separator to use in the csv file e.g. ',' or '\\t'")
     args = parser.parse_args()
-    bfabric_save_dataset2csv(out_dir=args.dir, dataset_id=args.id)
+    bfabric_save_dataset2csv(client=client, out_dir=args.dir, dataset_id=args.id, sep=args.sep)
 
 
 if __name__ == "__main__":
---
+++ bfabric/tests/integration/test_save_dataset2csv.py
@@ -0,0 +1,48 @@
+# TODO check escaping of "," and other special chars?
+import unittest
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pandas as pd
+
+from bfabric.bfabric2 import Bfabric
+from bfabric.scripts.bfabric_save_dataset2csv import bfabric_save_dataset2csv
+
+
+class TestSaveDataset2Csv(unittest.TestCase):
+    def setUp(self):
+        self.mock_client = Bfabric.from_config(config_env="TEST", verbose=True)
+        self.dataset_id = 46184
+
+    def test_save_dataset2csv(self):
+        with TemporaryDirectory() as temp_dir:
+            out_dir = Path(temp_dir)
+            bfabric_save_dataset2csv(self.mock_client, self.dataset_id, out_dir, sep=",")
+
+            expected_lines = [
+                r"Normal,Comma,Backslash,Apostrophe",
+                r"""just a normal string,"contains,some,commas,,,",testing\backslash/support,"Lot's""of""apostrophes"""
+                '"',
+            ]
+
+            out_file = out_dir / "dataset.csv"
+            actual_lines = out_file.read_text().splitlines()
+
+            self.assertListEqual(expected_lines, actual_lines)
+
+            df = pd.read_csv(out_file)
+            expected_df = pd.DataFrame(
+                [
+                    {
+                        "Normal": "just a normal string",
+                        "Comma": "contains,some,commas,,,",
+                        "Backslash": "testing\\backslash/support",
+                        "Apostrophe": 'Lot\'s"of"apostrophes',
+                    }
+                ]
+            )
+            pd.testing.assert_frame_equal(expected_df, df)
+
+
+if __name__ == "__main__":
+    unittest.main()
