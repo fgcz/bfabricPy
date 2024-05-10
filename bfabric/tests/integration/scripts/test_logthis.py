d9ba0b959f259a41bdb724f82b0dae9a1353da23
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-10 09:03:14 +0200

    wip

--- bfabric/tests/integration/scripts/test_logthis.py
+++ bfabric/tests/integration/scripts/test_logthis.py
@@ -5,22 +5,21 @@ from bfabric.bfabric2 import Bfabric
 from bfabric.tests.integration.integration_test_helper import DeleteEntities
 
 
-# TODO pending
 @unittest.skip
 class TestLogthis(unittest.TestCase):
     def setUp(self):
         self.client = Bfabric.from_config(config_env="TEST")
-        self.delete_entities = DeleteEntities(client=self.client, created_entities=[])
+        self.delete_entities = DeleteEntities(client=self.client)
         self.addCleanup(self.delete_entities)
         self.container_id = 3000
         self.ts = datetime.datetime.now().isoformat()
 
     def test_log(self):
         # create workunit
-        workunit_id = self.client.save(
+        workunit = self.client.save(
             "workunit", {"containerid": self.container_id, "name": f"Test {self.ts}", "applicationid": 1}
-        ).to_list_dict()[0]["id"]
-        self.delete_entities.created_entities.append(("workunit", workunit_id))
+        ).to_list_dict()[0]
+        self.delete_entities.register_entity(workunit)
 
         # create external job
         external_job_id = self.client.save(
