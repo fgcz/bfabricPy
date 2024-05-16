2115357375c6abbe1fb8cc70dc7e9dcfbc81a2f3
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-16 14:24:44 +0200

    delete unfinished mock_engine

--- bfabric/tests/exp/mock_engine.py
+++
@@ -1,8 +0,0 @@
-class MockEngine:
-    """This engine supports mocking requests."""
-
-    # TODO the main thing I want to mock:
-    #  - ResultContainer
-    #  - Ensuring the engine was called with the correct arguments
-    #  - Being able to update the ground truth easily
-    #  - Redacting sensitive data before adding it to the repository
