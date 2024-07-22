425ed4f8a5b8c0b76233930fa9cf1f2877ba90d0
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-07-22 08:42:54 +0200

    implement has_one relationship wrapper

---
+++ bfabric/entities/core/has_one.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from bfabric.entities.core.entity import Entity
+
+if TYPE_CHECKING:
+    from typing import Type
+
+
+class HasOne:
+    def __init__(self, entity: Type[Entity], *, bfabric_field: str) -> None:
+        self._entity_type = entity
+        self._bfabric_field = bfabric_field
+
+    def __get__(self, obj, objtype=None) -> Entity:
+        cache_attr = f"_HasOne__{self._bfabric_field}_cache"
+        if not hasattr(obj, cache_attr):
+            setattr(obj, cache_attr, self._load_entity(obj=obj))
+        return getattr(obj, cache_attr)
+
+    def _load_entity(self, obj) -> Entity:
+        client = obj._client
+        entity_data = obj.data_dict[self._bfabric_field]
+        return self._entity_type.find(id=entity_data["id"], client=client)
---
+++ bfabric/tests/unit/entities/core/test_has_one.py
@@ -0,0 +1,46 @@
+import pytest
+
+from bfabric import Bfabric
+from bfabric.entities.core.entity import Entity
+from bfabric.entities.core.has_one import HasOne
+
+
+class MockEntity(Entity):
+    def __init__(self, data_dict=None, client=None):
+        if data_dict is None:
+            data_dict = {"mock": "mock"}
+        super().__init__(data_dict=data_dict, client=client)
+
+
+@pytest.fixture()
+def mock_client(mocker):
+    return mocker.MagicMock(name="mock_client", spec=Bfabric)
+
+
+def test_init():
+    has_one = HasOne(MockEntity, bfabric_field="test_field")
+    assert has_one._entity_type == MockEntity
+    assert has_one._bfabric_field == "test_field"
+
+
+def test_get_when_cache_not_exists(mocker, mock_client):
+    mock_obj = MockEntity(data_dict={"test_field": {"id": 1}}, client=mock_client)
+    has_one = HasOne(MockEntity, bfabric_field="test_field")
+    mock_load_entity = mocker.patch.object(HasOne, "_load_entity", return_value="mock_entity")
+    result = has_one.__get__(mock_obj)
+    assert result == "mock_entity"
+    mock_load_entity.assert_called_once_with(obj=mock_obj)
+
+
+def test_get_when_cache_exists(mocker, mock_client):
+    mock_obj = MockEntity(data_dict={"test_field": {"id": 1}}, client=mock_client)
+    has_one = HasOne(MockEntity, bfabric_field="test_field")
+    mock_obj._HasOne__test_field_cache = "mock_entity"
+    mock_load_entity = mocker.patch.object(HasOne, "_load_entity", return_value="mock_entity")
+    result = has_one.__get__(mock_obj)
+    assert result == "mock_entity"
+    mock_load_entity.assert_not_called()
+
+
+if __name__ == "__main__":
+    pytest.main()
