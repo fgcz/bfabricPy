a088d1d87954a9468b4d6a46987cbd14c1bc3a4c
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-09 11:25:20 +0200

    add entities.Executable

--- bfabric/entities/__init__.py
+++ bfabric/entities/__init__.py
@@ -1,9 +1,10 @@
 from .application import Application
 from .dataset import Dataset
+from .executable import Executable
 from .external_job import ExternalJob
 from .parameter import Parameter
 from .resource import Resource
-from .workunit import Workunit
 from .storage import Storage
+from .workunit import Workunit
 
-__all__ = ["Application", "Dataset", "Parameter", "Resource", "Workunit", "ExternalJob", "Storage"]
+__all__ = ["Application", "Dataset", "Executable", "ExternalJob", "Parameter", "Resource", "Storage", "Workunit"]
