d56437f15fc8103ac63c570c53c95fedfed98f52
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-09 11:46:38 +0200

    add Order and Project

--- bfabric/entities/__init__.py
+++ bfabric/entities/__init__.py
@@ -2,9 +2,22 @@ from .application import Application
 from .dataset import Dataset
 from .executable import Executable
 from .external_job import ExternalJob
+from .order import Order
 from .parameter import Parameter
+from .project import Project
 from .resource import Resource
 from .storage import Storage
 from .workunit import Workunit
 
-__all__ = ["Application", "Dataset", "Executable", "ExternalJob", "Parameter", "Resource", "Storage", "Workunit"]
+__all__ = [
+    "Application",
+    "Dataset",
+    "Executable",
+    "ExternalJob",
+    "Order",
+    "Parameter",
+    "Project",
+    "Resource",
+    "Storage",
+    "Workunit",
+]
