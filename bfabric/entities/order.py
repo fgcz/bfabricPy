2fb6e3052d2e07c04bcfd324da4502d7563393ba
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-08-09 13:59:07 +0200

    add Order.project

--- bfabric/entities/order.py
+++ bfabric/entities/order.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from bfabric import Bfabric
 from bfabric.entities.core.entity import Entity
+from bfabric.entities.core.has_one import HasOne
 
 
 class Order(Entity):
@@ -11,3 +12,5 @@ class Order(Entity):
 
     def __init__(self, data_dict: dict[str, Any], client: Bfabric | None) -> None:
         super().__init__(data_dict=data_dict, client=client)
+
+    project = HasOne("Project", bfabric_field="project")
