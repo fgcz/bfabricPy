61b43d27dd499bc84bda393165457a11fcc940eb
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-03 08:48:00 +0200

    move prepare_folder function

--- bfabric/experimental/app_interface/input_preparation/__main__.py
+++ bfabric/experimental/app_interface/input_preparation/__main__.py
@@ -5,29 +5,7 @@ from pathlib import Path
 
 from bfabric.bfabric import Bfabric
 from bfabric.cli_formatting import setup_script_logging
-from bfabric.experimental.app_interface.input_preparation._spec import InputsSpec
-from bfabric.experimental.app_interface.input_preparation.prepare import PrepareInputs
-
-
-def prepare_folder(
-    inputs_yaml: Path, target_folder: Path | None, client: Bfabric, ssh_user: str | None, action: str = "prepare"
-) -> None:
-    # set defaults
-    inputs_yaml = inputs_yaml.absolute()
-    if target_folder is None:
-        target_folder = inputs_yaml.parent
-
-    # parse the specs
-    specs_list = InputsSpec.read_yaml(inputs_yaml)
-
-    # prepare the folder
-    prepare = PrepareInputs(client=client, working_dir=target_folder, ssh_user=ssh_user)
-    if action == "prepare":
-        prepare.prepare_all(specs=specs_list)
-    elif action == "clean":
-        prepare.clean_all(specs=specs_list)
-    else:
-        raise ValueError(f"Unknown action: {action}")
+from bfabric.experimental.app_interface.input_preparation.prepare import prepare_folder
 
 
 def main():
--- bfabric/experimental/app_interface/input_preparation/prepare.py
+++ bfabric/experimental/app_interface/input_preparation/prepare.py
@@ -9,7 +9,7 @@ from loguru import logger
 
 from bfabric.bfabric import Bfabric
 from bfabric.entities import Resource, Dataset
-from bfabric.experimental.app_interface.input_preparation._spec import ResourceSpec, DatasetSpec, SpecType
+from bfabric.experimental.app_interface.input_preparation._spec import ResourceSpec, DatasetSpec, SpecType, InputsSpec
 
 
 class PrepareInputs:
@@ -91,6 +91,27 @@ class PrepareInputs:
             path.unlink()
         else:
             logger.debug(f"Dataset {path} does not exist")
+
+
+def prepare_folder(
+    inputs_yaml: Path, target_folder: Path | None, client: Bfabric, ssh_user: str | None, action: str = "prepare"
+) -> None:
+    # set defaults
+    inputs_yaml = inputs_yaml.absolute()
+    if target_folder is None:
+        target_folder = inputs_yaml.parent
+
+    # parse the specs
+    specs_list = InputsSpec.read_yaml(inputs_yaml)
+
+    # prepare the folder
+    prepare = PrepareInputs(client=client, working_dir=target_folder, ssh_user=ssh_user)
+    if action == "prepare":
+        prepare.prepare_all(specs=specs_list)
+    elif action == "clean":
+        prepare.clean_all(specs=specs_list)
+    else:
+        raise ValueError(f"Unknown action: {action}")
 
 
 def _is_remote(path: str | Path) -> bool:
