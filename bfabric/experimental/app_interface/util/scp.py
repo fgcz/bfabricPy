1affc077be1fcda0bf7e91d9adb0b955b2083ea1
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-04 10:34:56 +0200

    improve scp wrapper

--- bfabric/experimental/app_interface/util/scp.py
+++ bfabric/experimental/app_interface/util/scp.py
@@ -7,9 +7,12 @@ from loguru import logger
 
 
 def scp(source: str | Path, target: str | Path, *, user: str | None = None) -> None:
-    """Performs scp source target.
+    """Copies a file using scp from source to target.
     Make sure that either the source or target specifies a host, otherwise you should just use shutil.copyfile.
+    If you copy to a folder, it is recommended to end the target with a slash (``/``).
     """
+    source = str(source)
+    target = str(target)
     source_remote = _is_remote(source)
     target_remote = _is_remote(target)
     if source_remote == target_remote:
@@ -22,8 +25,15 @@ def scp(source: str | Path, target: str | Path, *, user: str | None = None) -> N
         source = f"{user}@{source}"
     elif user and target_remote:
         target = f"{user}@{target}"
-    if not target_remote:
-        Path(target).parent.mkdir(parents=True, exist_ok=True)
+
+    # create output directory if necessary
+    if target_remote:
+        host, path = target.split(":", 1)
+        parent_path = str(Path(path).parent) if not path.endswith("/") else path
+        subprocess.run(["ssh", host, "mkdir", "-p", parent_path], check=True)
+    else:
+        parent_path = Path(target).parent if not target.endswith("/") else target
+        parent_path.mkdir(parents=True, exist_ok=True)
 
     logger.info(f"scp {source} {target}")
     subprocess.run(["scp", source, target], check=True)
