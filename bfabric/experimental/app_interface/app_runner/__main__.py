d2f09dbfb6ecb77123c986c2e89b0bb5956861f6
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-16 13:18:51 +0200

    move cli functionality

--- bfabric/experimental/app_interface/app_runner/__main__.py
+++ bfabric/experimental/app_interface/app_runner/__main__.py
@@ -3,20 +3,14 @@ from __future__ import annotations
 import argparse
 from pathlib import Path
 from typing import Union
-from venv import logger
 
 import yaml
-from pydantic import TypeAdapter, BaseModel
+from pydantic import TypeAdapter
 
 from bfabric.bfabric import Bfabric
 from bfabric.cli_formatting import setup_script_logging
 from bfabric.experimental.app_interface.app_runner._spec import AppSpec
-from bfabric.experimental.app_interface.app_runner.runner import Runner
-
-
-class ChunksFile(BaseModel):
-    # TODO move to better location
-    chunks: list[Path]
+from bfabric.experimental.app_interface.app_runner.runner import run_app
 
 
 def main() -> None:
@@ -30,17 +24,15 @@ def main() -> None:
     parser.add_argument("--ssh-user", type=str, required=False)
     parser.add_argument("--read-only", action="store_true")
     args = parser.parse_args()
-
     app_spec = AppSpec.model_validate(yaml.safe_load(args.app_spec.read_text()))
-    runner = Runner(spec=app_spec, client=client, ssh_user=args.ssh_user)
-    runner.run_dispatch(workunit_ref=args.workunit_ref, work_dir=args.work_dir)
-    chunks_file = ChunksFile.model_validate(yaml.safe_load((args.work_dir / "chunks.yml").read_text()))
-    for chunk in chunks_file.chunks:
-        logger.info(f"Processing chunk {chunk}")
-        runner.run_prepare_input(chunk_dir=chunk)
-        runner.run_process(chunk_dir=chunk)
-        if not args.read_only:
-            runner.run_register_outputs(chunk_dir=chunk, workunit_ref=args.workunit_ref)
+    run_app(
+        app_spec=app_spec,
+        workunit_ref=args.workunit_ref,
+        work_dir=args.work_dir,
+        client=client,
+        ssh_user=args.ssh_user,
+        read_only=args.read_only,
+    )
 
 
 if __name__ == "__main__":
--- bfabric/experimental/app_interface/app_runner/runner.py
+++ bfabric/experimental/app_interface/app_runner/runner.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 import shlex
 import subprocess
 from pathlib import Path
+from venv import logger
+
+import yaml
+from pydantic import BaseModel
 
 from bfabric import Bfabric
 from bfabric.experimental.app_interface.app_runner._spec import AppSpec
@@ -39,3 +43,27 @@ class Runner:
             client=self._client,
             ssh_user=self._ssh_user,
         )
+
+
+class ChunksFile(BaseModel):
+    # TODO move to better location
+    chunks: list[Path]
+
+
+def run_app(
+    app_spec: AppSpec,
+    workunit_ref: int | Path,
+    work_dir: Path,
+    client: Bfabric,
+    ssh_user: str | None = None,
+    read_only: bool = False,
+) -> None:
+    runner = Runner(spec=app_spec, client=client, ssh_user=ssh_user)
+    runner.run_dispatch(workunit_ref=workunit_ref, work_dir=work_dir)
+    chunks_file = ChunksFile.model_validate(yaml.safe_load((work_dir / "chunks.yml").read_text()))
+    for chunk in chunks_file.chunks:
+        logger.info(f"Processing chunk {chunk}")
+        runner.run_prepare_input(chunk_dir=chunk)
+        runner.run_process(chunk_dir=chunk)
+        if not read_only:
+            runner.run_register_outputs(chunk_dir=chunk, workunit_ref=args.workunit_ref)
