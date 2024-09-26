0a2317e39a1933fc6d9b2950c3be46f7fc302d52
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-09-26 13:43:33 +0200

    use relativepath for suffix identifcation

--- src/bfabric/scripts/bfabric_list_not_existing_storage_directories.py
+++ src/bfabric/scripts/bfabric_list_not_existing_storage_directories.py
@@ -18,16 +18,20 @@ from bfabric import Bfabric
 
 def list_not_existing_storage_dirs(client: Bfabric, root_dir: Path, technology_id: int | list[int]) -> None:
     """Lists not existing storage directories for a given technology id."""
-    results = client.read(endpoint="container", obj={"technologyid": technology_id}, return_id_only=True)
+    results = client.read(
+        endpoint="container",
+        obj={"technologyid": technology_id},
+        return_id_only=True,
+        max_results=500,
+    )
     container_ids = sorted({x["id"] for x in results})
-
     for container_id in container_ids:
         if not (root_dir / f"p{container_id}").is_dir():
             print(container_id)
 
 
 def main() -> None:
-    """Parses CLI arguments and calls `list_not_existing_storage_dirs`."""
+    """Calls `list_not_existing_storage_dirs`."""
     client = Bfabric.from_config()
     root_dir = Path("/srv/www/htdocs/")
     list_not_existing_storage_dirs(client=client, root_dir=root_dir, technology_id=[2, 4])
