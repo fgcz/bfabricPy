bf28baf678df57785940d6bf692258eaff8b2ac9
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-08 14:24:59 +0200

    unit test

---
+++ tests/bfabric/entities/test_externaljob.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+import pytest
+from pytest_mock import MockerFixture
+
+from bfabric.entities import ExternalJob, Workunit
+
+
+@pytest.fixture()
+def data_dict():
+    return {
+        "id": 1,
+        "cliententityclassname": "Workunit",
+        "cliententityid": 5,
+    }
+
+
+def test_workunit_when_available(mocker: MockerFixture, data_dict: dict[str, Any]):
+    mock_client = mocker.MagicMock(name="mock_client", spec=[])
+    mock_find = mocker.patch.object(Workunit, "find")
+    external_job = ExternalJob(data_dict, mock_client)
+    assert external_job.workunit == mock_find.return_value
+    mock_find.assert_called_once_with(id=5, client=mock_client)
+
+
+def test_workunit_when_wrong_class(mocker: MockerFixture, data_dict: dict[str, Any]):
+    mock_client = mocker.MagicMock(name="mock_client", spec=[])
+    mock_find = mocker.patch.object(Workunit, "find")
+    # TODO actually check which ones are the legal values here
+    data_dict["cliententityclassname"] = "WrongClass"
+    external_job = ExternalJob(data_dict, mock_client)
+    assert external_job.workunit is None
+    mock_find.assert_not_called()
