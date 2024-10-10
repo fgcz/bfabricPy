bdb8e50efacd441256a685286016674a675f69bd
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-10 13:45:18 +0200

    install less packages when testing

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,6 @@ nox.options.default_venv_backend = "uv"
 
 @nox.session
 def tests(session):
-    session.install(".[dev]")
+    session.install(".[test]")
     session.run("uv", "pip", "list")
     session.run("pytest")
