cd677cbb7983f4c3d5bc75a0cec899be7d627c25
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-10-23 16:03:30 +0200

    supply version info

--- app_runner/src/app_runner/cli/__main__.py
+++ app_runner/src/app_runner/cli/__main__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import importlib.metadata
+
 import cyclopts
 
 from app_runner.cli.app import app_app
@@ -8,7 +10,12 @@ from app_runner.cli.inputs import app_inputs
 from app_runner.cli.outputs import app_outputs
 from app_runner.cli.validate import app_validate
 
-app = cyclopts.App(help="Provides an entrypoint to app execution.\n\nFunctionality/API under active development!")
+package_version = importlib.metadata.version("app_runner")
+
+app = cyclopts.App(
+    help="Provides an entrypoint to app execution.\n\nFunctionality/API under active development!",
+    version=package_version,
+)
 app.command(app_inputs)
 app.command(app_outputs)
 app.command(app_app)
