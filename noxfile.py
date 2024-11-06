a5080d1121afc39b1523815758ab0a24fdf0f44f
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-06 13:40:53 +0100

    add session to build documentation

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,8 @@
 import nox
+import shutil
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
 
 nox.options.default_venv_backend = "uv"
 
@@ -8,3 +12,21 @@ def tests(session):
     session.install(".[test]")
     session.run("uv", "pip", "list")
     session.run("pytest")
+
+
+@nox.session(default=False)
+def docs(session):
+    """Builds documentation for bfabricPy and app-runner and writes to site directory."""
+    with TemporaryDirectory() as tmpdir:
+        session.install(".[doc]")
+        session.run("mkdocs", "build", "-d", Path(tmpdir) / "build_bfabricpy")
+
+        session.install("./app_runner[doc]")
+        session.run("sphinx-build", "-M", "html", "app_runner/docs", Path(tmpdir) / "build_app_runner")
+
+        target_dir = Path("site")
+        if target_dir.exists():
+            shutil.rmtree(target_dir)
+
+        shutil.copytree(Path(tmpdir) / "build_bfabricpy", target_dir)
+        shutil.copytree(Path(tmpdir) / "build_app_runner" / "html", target_dir / "app_runner")
