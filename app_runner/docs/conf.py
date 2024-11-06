cd040e01432f675c078cab3111a8ea7680fe4300
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-06 16:49:27 +0100

    build plantuml in container

--- app_runner/docs/conf.py
+++ app_runner/docs/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from pathlib import Path
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,10 +15,13 @@ author = "Leonardo Schwarz"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinxcontrib.autodoc_pydantic"]
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinxcontrib.autodoc_pydantic", "sphinxcontrib.plantuml"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+plantuml = str((Path(__file__).parent / "plantuml_wrapper.sh").absolute())
+plantuml_output_format = "svg"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
