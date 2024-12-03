1e9c04ead5417fb09329a481804dea984e34c118
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-03 08:35:50 +0100

    adapt unit test

--- tests/bfabric_scripts/test_bfabric_slurm_queue_status.py
+++ tests/bfabric_scripts/test_bfabric_slurm_queue_status.py
@@ -2,8 +2,8 @@ import polars as pl
 import polars.testing
 import pytest
 
-from bfabric.entities import Workunit
-from bfabric_scripts.bfabric_slurm_queue_status import get_slurm_jobs, get_workunit_status
+from bfabric.entities import Workunit, Application
+from bfabric_scripts.bfabric_slurm_queue_status import get_slurm_jobs, get_workunit_infos
 
 
 @pytest.fixture
@@ -32,16 +32,21 @@ def test_get_slurm_jobs_when_local(mocker, command_output):
     pl.testing.assert_frame_equal(df, expected_df)
 
 
-def test_get_workunit_status(mocker):
+def test_get_workunit_infos(mocker):
     mock_client = mocker.Mock(name="mock_client")
-    mock_find_all = mocker.patch.object(Workunit, "find_all")
+    mock_workunit_find_all = mocker.patch.object(Workunit, "find_all")
     workunit_ids = [5000, 5001]
-    mock_find_all.return_value = {
-        5001: Workunit({"id": 5000, "status": "RUNNING"}),
+    mock_workunit_find_all.return_value = {
+        5001: Workunit({"id": 5000, "status": "RUNNING", "application": {"id": 1}}),
     }
-    status = get_workunit_status(mock_client, workunit_ids)
-    assert status == {5000: "ZOMBIE", 5001: "RUNNING"}
-    mock_find_all.assert_called_once_with(ids=workunit_ids, client=mock_client)
+    mock_app_find_all = mocker.patch.object(Application, "find_all")
+    mock_app_find_all.return_value = {1: {"id": 1, "name": "myapp"}}
+    infos = get_workunit_infos(mock_client, workunit_ids)
+    assert infos == [
+        {"workunit_id": 5000, "status": "ZOMBIE", "application_name": "N/A"},
+        {"workunit_id": 5001, "status": "RUNNING", "application_name": "myapp"},
+    ]
+    mock_workunit_find_all.assert_called_once_with(ids=workunit_ids, client=mock_client)
 
 
 if __name__ == "__main__":
