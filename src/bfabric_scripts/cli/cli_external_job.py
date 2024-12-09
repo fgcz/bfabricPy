09e73069f7603e142daff823e55224aab2243e1a
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-09 12:36:11 +0100

    add initial external job interface

---
+++ src/bfabric_scripts/cli/cli_external_job.py
@@ -0,0 +1,15 @@
+from typing import Literal
+
+import cyclopts
+
+from bfabric import Bfabric
+from bfabric.wrapper_creator.bfabric_submitter import BfabricSubmitter
+
+app = cyclopts.App()
+
+
+@app.command
+def submit(external_job_id: int, scheduler: Literal["Slurm"] = "Slurm") -> None:
+    client = Bfabric.from_config()
+    submitter = BfabricSubmitter(client=client, externaljobid=external_job_id, scheduleroot="/usr/", scheduler="Slurm")
+    submitter.submitter_yaml()
