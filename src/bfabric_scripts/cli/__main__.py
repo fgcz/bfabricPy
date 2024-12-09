4ec35982df855b39db0bd2c32ef601e504bd323c
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-09 12:36:11 +0100

    add initial external job interface

--- src/bfabric_scripts/cli/__main__.py
+++ src/bfabric_scripts/cli/__main__.py
@@ -1,9 +1,11 @@
 import cyclopts
-from bfabric_scripts.cli.cli_read import app as _app_read
 
+from bfabric_scripts.cli.cli_external_job import app as _app_external_job
+from bfabric_scripts.cli.cli_read import app as _app_read
 
 app = cyclopts.App()
 app.command(_app_read, name="read")
+app.command(_app_external_job, name="external_job")
 
 if __name__ == "__main__":
     app()
