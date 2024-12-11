7c0c5b4629863346df516b7db088f3b2800f8b49
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-11 12:44:31 +0100

    set the slurm root dynamically

--- src/bfabric_scripts/cli/cli_external_job.py
+++ src/bfabric_scripts/cli/cli_external_job.py
@@ -1,3 +1,5 @@
+import shutil
+from pathlib import Path
 from typing import Literal
 
 import cyclopts
@@ -8,10 +10,21 @@ from bfabric.wrapper_creator.bfabric_submitter import BfabricSubmitter
 app = cyclopts.App()
 
 
+def find_slurm_root() -> str:
+    sbatch_path = shutil.which("sbatch")
+    if sbatch_path:
+        return str(Path(sbatch_path).parents[1])
+    else:
+        return "/usr/"
+
+
 @app.command
 def submitter(external_job_id: int, scheduler: Literal["Slurm"] = "Slurm") -> None:
+    slurm_root = find_slurm_root()
     client = Bfabric.from_config()
-    submitter = BfabricSubmitter(client=client, externaljobid=external_job_id, scheduleroot="/usr/", scheduler="Slurm")
+    submitter = BfabricSubmitter(
+        client=client, externaljobid=external_job_id, scheduleroot=slurm_root, scheduler="Slurm"
+    )
     submitter.submitter_yaml()
 
 
