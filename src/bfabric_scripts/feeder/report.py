f8d0ca2b9957d9e7c2c958ba2c4154d7ea3835af
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-13 14:13:43 +0100

    replace bfabric_feeder.py with single function in bfabric_scripts

---
+++ src/bfabric_scripts/feeder/report.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from loguru import logger
+from rich.pretty import pprint
+
+from bfabric import Bfabric
+from bfabric.entities import Resource
+from bfabric_scripts.feeder.file_attributes import get_file_attributes
+
+
+def report_resource(client: Bfabric, resource_id: int) -> None:
+    """Saves the provided resource's checksum, file size and available state."""
+    resource = Resource.find(id=resource_id, client=client)
+    pprint(resource, indent_guides=False)
+
+    if not hasattr(resource, "storage"):
+        # TODO is this possible for a resource to not have a storage?
+        logger.error("Resource does not have a storage")
+        return
+
+    filename = Path(resource.storage["basepath"]) / resource["relativepath"]
+    if filename.is_file():
+        checksum, _, filesize, _ = get_file_attributes(str(filename))
+        client.save("resource", {"id": resource_id, "size": filesize, "status": "available", "filechecksum": checksum})
+    else:
+        client.save("resource", {"id": resource_id, "status": "failed"})
