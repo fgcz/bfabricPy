231a5737f3d787688ee616eec95bff847d266a25
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-12-17 13:13:43 +0100

    delete old cli_log.py

--- src/bfabric_scripts/cli/cli_log.py
+++
@@ -1,9 +0,0 @@
-import cyclopts
-
-from bfabric_scripts.cli.log.log_write import cmd as _cmd_log_write
-
-# TODO one thing to consider before this is fully ready is maybe it should be merged into the external-job command
-#      which currently already contains a lot of system functionality
-
-app = cyclopts.App()
-app.command(_cmd_log_write, name="write")
