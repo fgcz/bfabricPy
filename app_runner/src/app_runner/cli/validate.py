786380ae0a618d3f6dd145e3cf8691571ed4f8df
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-01-08 15:12:56 +0100

    clean

--- app_runner/src/app_runner/cli/validate.py
+++ app_runner/src/app_runner/cli/validate.py
@@ -3,9 +3,8 @@ from __future__ import annotations
 from pathlib import Path
 
 import cyclopts
-import rich
-import rich.pretty
 import yaml
+from rich.pretty import pprint
 
 from app_runner.specs.app.app_spec import AppSpecFile, AppVersions
 from app_runner.specs.inputs_spec import InputsSpec
@@ -19,32 +18,32 @@ app_validate = cyclopts.App("validate", help="Validate yaml files.")
 def app_spec_file(yaml_file: Path) -> None:
     """Validate an app spec file."""
     app_spec_file = AppSpecFile.model_validate(yaml.safe_load(yaml_file.read_text()))
-    rich.pretty.pprint(app_spec_file)
+    pprint(app_spec_file)
 
 
 @app_validate.command()
 def app_versions(app_yaml: Path, submitters_yaml: Path, app_id: str = "x", app_name: str = "y") -> None:
     """Validates the app versions by expanding the relevant config info."""
     versions = AppVersions.load_yaml(app_yaml, submitters_yaml=submitters_yaml, app_id=app_id, app_name=app_name)
-    rich.pretty.pprint(versions)
+    pprint(versions)
 
 
 @app_validate.command()
 def inputs_spec(yaml_file: Path) -> None:
     """Validate an inputs spec file."""
     inputs_spec = InputsSpec.model_validate(yaml.safe_load(yaml_file.read_text()))
-    rich.pretty.pprint(inputs_spec)
+    pprint(inputs_spec)
 
 
 @app_validate.command()
 def outputs_spec(yaml_file: Path) -> None:
     """Validate an outputs spec file."""
     outputs_spec = OutputsSpec.model_validate(yaml.safe_load(yaml_file.read_text()))
-    rich.pretty.pprint(outputs_spec)
+    pprint(outputs_spec)
 
 
 @app_validate.command()
 def submitters_spec(yaml_file: Path) -> None:
     """Validate a submitters spec file."""
     submitters_spec = SubmittersSpec.model_validate(yaml.safe_load(yaml_file.read_text()))
-    rich.pretty.pprint(submitters_spec)
+    pprint(submitters_spec)
