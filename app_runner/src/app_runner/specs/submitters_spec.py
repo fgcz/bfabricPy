b9e6c10328615d16e209f27dfca20a25cbc6db6e
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-01-16 16:07:21 +0100

    add some implementation for the submitters_spec.py

--- app_runner/src/app_runner/specs/submitters_spec.py
+++ app_runner/src/app_runner/specs/submitters_spec.py
@@ -1,6 +1,37 @@
-from pydantic import BaseModel
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from app_runner.specs.config_interpolation import Variables, interpolate_config_strings
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Literal
+
+
+class SubmitterSlurmConfig(BaseModel):
+    local_script_dir: Path = Field(alias="local-script-dir")
+    worker_scratch_dir: Path = Field(alias="worker-scratch-dir")
+
+
+class SubmitterSlurm(BaseModel):
+    model_config = ConfigDict(coerce_numbers_to_str=True)
+
+    type: Literal["slurm"]
+    params: dict[str, str | None]
+    config: SubmitterSlurmConfig
+
+
+class SubmittersSpecTemplate(BaseModel):
+    submitters: dict[str, SubmitterSlurm]
+
+    def evaluate(self, variables: Variables) -> SubmittersSpec:
+        data_template = self.model_dump(mode="json")
+        data = interpolate_config_strings(data_template, variables=variables.model_dump(mode="python"))
+        return SubmittersSpec.model_validate(data)
 
 
 class SubmittersSpec(BaseModel):
-    # TODO
-    submitters: dict[str, None]
+    submitters: dict[str, SubmitterSlurm]
