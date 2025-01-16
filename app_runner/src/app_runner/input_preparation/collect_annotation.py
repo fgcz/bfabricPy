baea4317607ed083a7d2a7d55ec8742db449cb12
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-01-16 08:47:38 +0100

    create the correct parent directory

--- app_runner/src/app_runner/input_preparation/collect_annotation.py
+++ app_runner/src/app_runner/input_preparation/collect_annotation.py
@@ -28,9 +28,10 @@ def collect_resource_sample_annotation(spec: BfabricAnnotationResourceSampleSpec
 
 def prepare_annotation(spec: BfabricAnnotationSpec, client: Bfabric, working_dir: Path) -> None:
     """Prepares the annotation specified by the spec and writes it to the specified location."""
-    Path(spec.filename).parent.mkdir(parents=True, exist_ok=True)
+    path = working_dir / spec.filename
+    path.parent.mkdir(parents=True, exist_ok=True)
     match spec.annotation:
         case "resource_sample":
-            collect_resource_sample_annotation(spec, client=client, path=working_dir / spec.filename)
+            collect_resource_sample_annotation(spec, client=client, path=path)
         case _:
             raise ValueError(f"Unsupported annotation type: {spec.annotation}")
