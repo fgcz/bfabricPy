a384d0e77353e6d46d44c45d210b4ef6951812f7
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-01-21 11:32:51 +0100

    submitter parameter parsing

--- app_runner/src/app_runner/submitter/slurm_submitter.py
+++ app_runner/src/app_runner/submitter/slurm_submitter.py
@@ -2,37 +2,15 @@ from __future__ import annotations
 
 import os
 import subprocess
-from enum import Enum
 from typing import TYPE_CHECKING
 
 import yaml
 from loguru import logger
-from pydantic import BaseModel
 
 if TYPE_CHECKING:
     from app_runner.specs.submitters_spec import SubmitterSlurmSpec
     from app_runner.bfabric_app.workunit_wrapper_data import WorkunitWrapperData
-
-
-class SlurmSubmitterSpecialStrings(Enum):
-    default = "[default]"
-    auto = "[auto]"
-
-    @classmethod
-    def parse(cls, string: str) -> SlurmSubmitterSpecialStrings | None:
-        if string == cls.default.value:
-            return cls.default
-        elif string == cls.auto.value:
-            return cls.auto
-        else:
-            return None
-
-
-class SlurmSubmitterUserParams(BaseModel):
-    partition: str
-    nodeslist: str
-    memory: str
-
+    from app_runner.submitter.slurm_submitter_params import SlurmSubmitterParams
 
 _MAIN_BASH_TEMPLATE = """
 tee app_version.yml <<YAML
@@ -53,16 +31,11 @@ class SlurmSubmitter:
     def __init__(self, default_config: SubmitterSlurmSpec) -> None:
         self._default_config = default_config
 
-    def _get_concrete_params(self, specific_params: dict[str, str | None]) -> dict[str, str]:
-        merged = {**self._default_config.params, **specific_params}
-        return {key: value for key, value in merged.items() if value is not None}
-
     def _compose_script_header(self, concrete_params: dict[str, str]) -> str:
         return "\n".join(["#!/bin/bash"] + [f"#SBATCH {key}={value}" for key, value in concrete_params.items()])
 
-    def _compose_script(self, main_command: str, specific_params: dict[str, str | None]) -> str:
-        concrete_params = self._get_concrete_params(specific_params)
-        script_header = self._compose_script_header(concrete_params)
+    def _compose_script(self, main_command: str, submitter_params: SlurmSubmitterParams) -> str:
+        script_header = self._compose_script_header(submitter_params.sbatch_params)
         return f"{script_header}\n\n{main_command}"
 
     def _interpolate_main(self, app_version_yml: str, workunit_definition_yml: str, app_runner_version: str) -> str:
@@ -82,14 +55,19 @@ class SlurmSubmitter:
             app_runner_version=app_runner_version,
         )
 
-    def submit(self, workunit_wrapper_data: WorkunitWrapperData, specific_params: dict[str, str | None]) -> None:
+    # TODO -> how to implement this cleanly
+    # def get_scratch_dir(self, workunit_wrapper_data: WorkunitWrapperData, submitter_params: SlurmSubmitterParams):
+    #    return mako.template.Template(template).render(app={"app": TODO})
+
+    def submit(self, workunit_wrapper_data: WorkunitWrapperData, submitter_params: SlurmSubmitterParams) -> None:
         # Determine the script path
         workunit_id = workunit_wrapper_data.workunit_definition.registration.workunit_id
         script_path = self._default_config.config.local_script_dir / f"workunitid-{workunit_id}_run.bash"
 
         # Generate the script
         main_command = self._get_main_command(workunit_wrapper_data=workunit_wrapper_data)
-        script = self._compose_script(main_command=main_command, specific_params=specific_params)
+        # TODO config should be merged in a standard way
+        script = self._compose_script(main_command=main_command, submitter_params=submitter_params)
         script_path.write_text(script)
         script_path.chmod(0o755)
 
