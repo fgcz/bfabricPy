f8f4f8ca1a97f90e8ab5ad6e646a984cc0e4aa3f
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-01-21 13:55:28 +0100

    make app name safe

--- app_runner/src/app_runner/specs/config_interpolation.py
+++ app_runner/src/app_runner/specs/config_interpolation.py
@@ -17,7 +17,7 @@ class VariablesApp(BaseModel):
     name: str
     version: str
 
-    @field_validator("name")
+    @field_validator("name", mode="before")
     def validate_safe_name(cls, value: str) -> str:
         characters = re.compile(r"^[a-zA-Z0-9_-]+$")
         return characters.sub("_", value)
