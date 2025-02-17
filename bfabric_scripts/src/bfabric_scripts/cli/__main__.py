d1d6086cdf962175d007d0666a64e3b861e79be0
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-02-17 17:20:50 +0100

    add package version information

--- bfabric_scripts/src/bfabric_scripts/cli/__main__.py
+++ bfabric_scripts/src/bfabric_scripts/cli/__main__.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 import cyclopts
 
 from bfabric_scripts.cli.cli_api import cmd_api
@@ -6,7 +8,9 @@ from bfabric_scripts.cli.cli_executable import cmd_executable
 from bfabric_scripts.cli.cli_external_job import app as _app_external_job
 from bfabric_scripts.cli.cli_workunit import cmd_workunit
 
-app = cyclopts.App()
+package_version = importlib.metadata.version("bfabric_scripts")
+
+app = cyclopts.App(version=package_version)
 app.command(cmd_api, name="api")
 app.command(cmd_dataset, name="dataset")
 app.command(cmd_executable, name="executable")
