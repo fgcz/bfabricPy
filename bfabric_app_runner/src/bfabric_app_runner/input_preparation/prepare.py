8adbf232129d67e17f27d460deddafd87ac9166b
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-03-06 15:07:25 +0100

    start implementing

--- bfabric_app_runner/src/bfabric_app_runner/input_preparation/prepare.py
+++ bfabric_app_runner/src/bfabric_app_runner/input_preparation/prepare.py
@@ -4,10 +4,12 @@ from typing import TYPE_CHECKING, Literal
 
 from bfabric_app_runner.input_preparation.integrity import IntegrityState
 from bfabric_app_runner.input_preparation.list_inputs import list_input_states
+from bfabric_app_runner.inputs.resolve.resolver import Resolver
 from bfabric_app_runner.specs.inputs.bfabric_dataset_spec import BfabricDatasetSpec
 from bfabric_app_runner.specs.inputs.bfabric_order_fasta_spec import BfabricOrderFastaSpec
 from bfabric_app_runner.specs.inputs.bfabric_resource_spec import BfabricResourceSpec
 from bfabric_app_runner.specs.inputs.file_spec import FileSpec
+from bfabric_app_runner.specs.inputs.static_file_spec import StaticFileSpec
 from bfabric_app_runner.specs.inputs.static_yaml_spec import StaticYamlSpec
 from bfabric_app_runner.specs.inputs_spec import (
     InputSpecType,
@@ -15,10 +17,10 @@ from bfabric_app_runner.specs.inputs_spec import (
 )
 from loguru import logger
 
-
 if TYPE_CHECKING:
     from pathlib import Path
     from bfabric.bfabric import Bfabric
+    from bfabric_app_runner.inputs.resolve.resolved_inputs import ResolvedInputs
 
 
 class PrepareInputs:
@@ -89,15 +91,58 @@ def prepare_folder(
     inputs_spec = InputsSpec.read_yaml(inputs_yaml)
 
     if filter:
-        inputs_spec = inputs_spec.apply_filter(filter, client=client)
-        if not inputs_spec.inputs:
-            raise ValueError(f"Filter {filter} did not match any input files")
+        # TODO this will require filenames which we only have once its resolved
+        raise NotImplementedError
+        # inputs_spec = inputs_spec.apply_filter(filter, client=client)
+        # if not inputs_spec.inputs:
+        #    raise ValueError(f"Filter {filter} did not match any input files")
+
+    # resolve the specs
+    resolver = Resolver(client=client)
+    input_files = resolver.resolve(specs=inputs_spec.inputs)
 
     # prepare the folder
-    prepare = PrepareInputs(client=client, working_dir=target_folder, ssh_user=ssh_user)
+    # prepare = PrepareInputs(client=client, working_dir=target_folder, ssh_user=ssh_user)
     if action == "prepare":
-        prepare.prepare_all(specs=inputs_spec.inputs)
+        prepare_input_files(input_files=input_files, working_dir=target_folder, ssh_user=ssh_user)
     elif action == "clean":
-        prepare.clean_all(specs=inputs_spec.inputs)
+        raise NotImplementedError
     else:
         raise ValueError(f"Unknown action: {action}")
+
+
+def prepare_input_files(input_files: ResolvedInputs, working_dir: Path, ssh_user: str | None) -> None:
+    """Prepares the input files in the working directory according to the provided specs."""
+    for input_file in input_files.files:
+        match input_file:
+            case FileSpec() as file_spec:
+                _prepare_file_spec(spec=file_spec, path=working_dir, ssh_user=ssh_user)
+            case StaticFileSpec(content=content, filename=filename):
+                _write_file_if_changed(content=content, path=working_dir / filename)
+
+
+def _prepare_file_spec(spec: FileSpec, path: Path, ssh_user: str | None) -> None:
+    raise NotImplementedError
+
+
+def _write_file_if_changed(content: str | bytes, path: Path) -> None:
+    binary_flag = "b" if isinstance(content, bytes) else ""
+
+    # ensure dir exists
+    path.parent.mkdir(exist_ok=True, parents=True)
+
+    # check if the file exists
+    if path.exists():
+        if not path.is_file():
+            msg = f"Path {path} exists but is not a file"
+            raise ValueError(msg)
+        with path.open(f"r{binary_flag}") as f:
+            existing_content = f.read()
+        if existing_content == content:
+            logger.debug(f"Skipping {path} as it already exists and has the same content")
+            return
+
+    # write the file
+    with path.open(f"w{binary_flag}") as f:
+        f.write(content)
+    logger.info(f"Writen to {path}")
