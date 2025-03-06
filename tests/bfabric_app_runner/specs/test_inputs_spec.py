5e9bb853297691ba1c356e1a6d58d0cbcb1426e8
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-03-06 16:45:51 +0100

    delete

--- tests/bfabric_app_runner/specs/test_inputs_spec.py
+++ tests/bfabric_app_runner/specs/test_inputs_spec.py
@@ -1,7 +1,6 @@
 import pytest
 import yaml
 
-from bfabric.entities import Resource
 from bfabric_app_runner.specs.inputs.bfabric_dataset_spec import BfabricDatasetSpec
 from bfabric_app_runner.specs.inputs.bfabric_resource_spec import BfabricResourceSpec
 from bfabric_app_runner.specs.inputs_spec import InputsSpec
@@ -44,24 +43,3 @@ def test_serialize(parsed, serialized):
 
 def test_parse(parsed, serialized):
     assert InputsSpec.model_validate(yaml.safe_load(serialized)) == parsed
-
-
-def test_resolve_filename_with_filename(mocker):
-    """Test when filename is provided"""
-    client = mocker.Mock(name="client")
-    spec = BfabricResourceSpec(id=1, filename="test.txt", check_checksum=True)
-
-    result = spec.resolve_filename(client)
-    assert result == "test.txt"
-
-
-def test_resolve_filename_without_filename(mocker):
-    """Test when filename is not provided"""
-    client = mocker.MagicMock(name="client")
-    mock_resource_find = mocker.patch.object(Resource, "find", return_value={"relativepath": "/path/to/data.csv"})
-
-    spec = BfabricResourceSpec(id=1, check_checksum=True)
-
-    result = spec.resolve_filename(client)
-    assert result == "data.csv"
-    mock_resource_find.assert_called_once_with(id=1, client=client)
