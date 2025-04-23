e01af3adb13025c57e3ad2c7cc7b6f32f2bb73f7
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-04-23 09:49:17 +0200

    clean

--- bfabric/src/bfabric/config/config_data.py
+++ bfabric/src/bfabric/config/config_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from pydantic import BaseModel
 
-from bfabric import BfabricClientConfig, BfabricAuth
+from bfabric.config import BfabricClientConfig, BfabricAuth
 from bfabric.config.config_file import read_config
 
 
@@ -13,18 +13,24 @@ class ConfigData(BaseModel):
     auth: BfabricAuth | None
 
 
-def load_config_data(config_path: Path | str | None = None) -> ConfigData:
-    """Loads the configuration data."""
-    if "BFABRICPY_CONFIG_DATA" in os.environ:
-        return ConfigData.model_validate_json(os.environ["BFABRICPY_CONFIG_DATA"])
-
+def _read_config_file(config_path: Path | str | None, fallback_config_env: str | None) -> ConfigData:
     config_file_path = Path(config_path or "~/.bfabricpy.yml").expanduser()
     if not config_file_path.is_file():
         msg = f"No explicit config provided, and no config file found at {config_file_path}"
         raise OSError(msg)
 
-    config, auth = read_config(config_path=config_file_path, config_env=os.environ.get("BFABRICPY_CONFIG_ENV"))
+    config, auth = read_config(
+        config_path=config_file_path, config_env=fallback_config_env or os.environ.get("BFABRICPY_CONFIG_ENV")
+    )
     return ConfigData(client=config, auth=auth)
+
+
+def load_config_data(config_path: Path | str | None = None, fallback_config_env: str | None = None) -> ConfigData:
+    """Loads the configuration data."""
+    if "BFABRICPY_CONFIG_DATA" in os.environ:
+        return ConfigData.model_validate_json(os.environ["BFABRICPY_CONFIG_DATA"])
+    else:
+        return _read_config_file(config_path=config_path, fallback_config_env=fallback_config_env)
 
 
 def export_config_data(config_data: ConfigData) -> str:
