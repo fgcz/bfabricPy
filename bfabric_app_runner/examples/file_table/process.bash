a42ab45468653f14bf909e67bf5aa4f66a5fa9ff
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-04-24 15:43:31 +0200

    impl?

--- bfabric_app_runner/examples/file_table/process.bash
+++ bfabric_app_runner/examples/file_table/process.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 chunk_dir="$(realpath $1)"
-config_dir=/Users/leo/code/bfabricPy/bfabric_app_runner/examples/file_table
-uv run --locked --project /Users/leo/code/bfabricPy/bfabric_app_runner/examples/file_table
+config_dir=/scratch/leo/code/bfabricPy/bfabric_app_runner/examples/file_table
+uv run --locked --project "$config_dir" \
   snakemake -p -s "$config_dir/Snakefile" --cores 4 -d "$chunk_dir" outputs.yml
