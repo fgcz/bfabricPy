96bf08c8e10933187172ebeb564dd9c82bf3bedd
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-02 17:07:08 +0200

    initial skeleton

--- bfabric_app_runner/src/bfabric_app_runner/cli_uv/__main__.py
+++ bfabric_app_runner/src/bfabric_app_runner/cli_uv/__main__.py
@@ -1,7 +1,9 @@
+import sys
+from pathlib import Path
+
 import cyclopts
 
-app = cyclopts.App()
-
+from bfabric_app_runner.cli_uv.wheel import infer_app_module_from_wheel
 
 # @app.default
 # def dispatch():
@@ -34,3 +36,54 @@ app = cyclopts.App()
 #        #  alternatively we could hack it at the end depending on the command (make it toggleable maybe)
 #    ]
 #
+
+
+app_dispatch = cyclopts.App(name="bfabric-app-runner-uv dispatch")
+
+
+@app_dispatch.default()
+def cmd_dispatch(
+    app_pkg: Path,
+    work_dir: Path,
+    workunit_ref: int | Path,
+    app_module: str | None = None,
+) -> None:
+    """Dispatches a bfabric-app-runner environment for the provided package."""
+    if app_module is None:
+        app_module = infer_app_module_from_wheel(app_pkg)
+
+    pass
+
+
+def handle_run(argv: list[str]) -> None:
+    """Handles the main run commands, which will be executed in the managed bfabric-app-runner environment."""
+    pass
+
+
+def handle_cli(argv: list[str]) -> None:
+    """Handles the command line interface for the bfabric-app-runner-uv CLI.
+
+    Since we want to dispatch the command to the managed `bfabric-app-runner` in some cases, this function
+    performs some direct handling of the command line arguments. This is kind of restrictive compared
+    to other commands, but gives us the most flexibility for now.
+
+    TODO this might be reconsidered later. maybe it could be done cleaner with cyclopts too
+    """
+    argv = argv[1:]
+
+    if argv[0] == "run":
+        # send the command ot the bfabric-app-runner env
+        handle_run(argv[1:])
+    elif argv[0] == "dispatch":
+        app_dispatch(argv[1:])
+    else:
+        raise ValueError(f"Unknown command: {argv[0]}")
+
+
+def main() -> None:
+    """Main entry point for the `bfabric-app-runner-uv` CLI."""
+    handle_cli(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
