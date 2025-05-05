a5a26fa899d829118eb9bc6d23c30bdc0798b252
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-05 13:48:29 +0200

    impl is_wheel_reference

--- bfabric_app_runner/src/bfabric_app_runner/cli_uv/wheel_info.py
+++ bfabric_app_runner/src/bfabric_app_runner/cli_uv/wheel_info.py
@@ -3,6 +3,12 @@ from pathlib import Path
 from wheel_filename import parse_wheel_filename
 
 
+def is_wheel_reference(deps_string: str) -> bool:
+    """Returns `True` if the specified dependency string is a pure wheel reference."""
+    # TODO this might not completely cover all things supported by `uv run --with`
+    return deps_string.lower().endswith(".whl") and "," not in deps_string
+
+
 def infer_app_module_from_wheel(app_pkg: Path) -> str:
     """Given a wheel file, infers the module name which can be used for the app dispatch."""
     file_name = Path(app_pkg).name
--- tests/bfabric_app_runner/cli_uv/test_wheel_info.py
+++ tests/bfabric_app_runner/cli_uv/test_wheel_info.py
@@ -1,6 +1,21 @@
 from pathlib import Path
 
-from bfabric_app_runner.cli_uv.wheel_info import infer_app_module_from_wheel
+import pytest
+
+from bfabric_app_runner.cli_uv.wheel_info import infer_app_module_from_wheel, is_wheel_reference
+
+
+@pytest.mark.parametrize(
+    "deps_string, expected_val",
+    [
+        ("my_package-1.2.3.dev9-py3-none-any.whl", True),
+        ("bfabric,my_package-1.2.3.dev9-py3-none-any.whl", False),
+        ("numpy,polars", False),
+        ("numpy", False),
+    ],
+)
+def test_is_wheel_reference(deps_string: str, expected_val: bool):
+    assert is_wheel_reference(deps_string) == expected_val
 
 
 def test_infer_app_module_from_wheel():
