825837a25eab47d500f7037853988eafc6f8c24a
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-16 15:03:49 +0200

    freeze initial code

---
+++ bfabric_app_runner/src/bfabric_app_runner/simple_submitter/dummy_wrapper_creator.py
@@ -0,0 +1,10 @@
+from loguru import logger
+
+
+def main() -> None:
+    """Dummy main function."""
+    logger.success("Dummy wrapper does not do anything.")
+
+
+if __name__ == "__main__":
+    main()
