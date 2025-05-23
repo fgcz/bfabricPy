d0d22294d93922d368f7ea1c7d054ad93d7c4eef
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2025-05-23 15:50:15 +0200

    Add `bfabric-cli executable dump` command (#203)

--- bfabric/src/bfabric/entities/executable.py
+++ bfabric/src/bfabric/entities/executable.py
@@ -5,16 +5,19 @@ from functools import cached_property
 from typing import Any, TYPE_CHECKING
 
 from bfabric.entities.core.entity import Entity
+from bfabric.entities.core.has_many import HasMany
 from bfabric.entities.core.has_one import HasOne
 
 if TYPE_CHECKING:
     from bfabric import Bfabric
+    from bfabric.entities import Parameter
 
 
 class Executable(Entity):
     ENDPOINT = "executable"
 
     storage = HasOne(entity="Storage", bfabric_field="storage", optional=True)
+    parameters: HasMany[Parameter] = HasMany(entity="Parameter", bfabric_field="parameter", optional=True)
 
     def __init__(self, data_dict: dict[str, Any], client: Bfabric | None = None) -> None:
         super().__init__(data_dict=data_dict, client=client)
