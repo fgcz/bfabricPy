ec13a487279bbc9c44b373274d80e87ad505c25d
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-28 14:40:11 +0200

    clean

--- bfabric_app_runner/src/bfabric_app_runner/slurm_submitter/app_runner_wrapper_template.py
+++ bfabric_app_runner/src/bfabric_app_runner/slurm_submitter/app_runner_wrapper_template.py
@@ -7,7 +7,6 @@ import mako.template
 from loguru import logger
 from pydantic import BaseModel
 
-from bfabric.utils.path_safe_name import path_safe_name
 
 if TYPE_CHECKING:
     from bfabric.entities import Workunit
@@ -21,7 +20,6 @@ class AppRunnerWrapperTemplate:
             bfabric_app_runner: str = "bfabric-app-runner==0.0.22"
 
         dependencies: Dependencies
-        working_directory: str
         workunit_id: int
         app_yaml_path: str
 
@@ -31,16 +29,8 @@ class AppRunnerWrapperTemplate:
             dependencies = cls.Params.Dependencies()
 
             app_yaml_path = workunit.application["program"]
-            # TODO hermetic isolation by B-Fabric instance!
-            # TODO customizable
-            # TODO this does not have the legacy logic of rediscovering existing folders
-            app_name = path_safe_name(workunit.application["name"])
-            app_id = workunit.application.id
-            working_directory = f"/scratch/A{app_id}_{app_name}/WU{workunit.id}"
-
             return cls.Params(
                 dependencies=dependencies,
-                working_directory=working_directory,
                 workunit_id=workunit.id,
                 app_yaml_path=app_yaml_path,
             )
