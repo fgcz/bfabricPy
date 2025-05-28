9548a29f3846d84bb08a0b9ffa804f496ca76df4
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-28 13:29:19 +0200

    renames

--- bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_params.py
+++ bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_params.py
@@ -14,7 +14,7 @@ from bfabric_app_runner.simple_submitter.config.slurm_workunit_params import (
 from bfabric_app_runner.specs.config_interpolation import VariablesApp, VariablesWorkunit, interpolate_config_strings
 
 
-class _SlurmConfigBase(BaseModel):
+class _SlurmParametersBase(BaseModel):
     # Note: Originally, the idea was that at the app level app_params could also be defined, but this was omitted for
     #       simplicity in this version.
     submitter_params: dict[str, str | None]
@@ -23,29 +23,29 @@ class _SlurmConfigBase(BaseModel):
     """Allows setting a controlled set of parameters."""
 
 
-class SlurmConfig(_SlurmConfigBase):
+class SlurmParameters(_SlurmParametersBase):
     @cached_property
     def sbatch_params(self) -> dict[str, str]:
         merged = {**self.submitter_params, **self.workunit_params.as_dict()}
         return {key: value for key, value in merged.items() if value is not None}
 
 
-class SlurmConfigTemplate(_SlurmConfigBase):
-    def evaluate(self, app: VariablesApp, workunit: VariablesWorkunit) -> SlurmConfig:
+class SlurmParametersTemplate(_SlurmParametersBase):
+    def evaluate(self, app: VariablesApp, workunit: VariablesWorkunit) -> SlurmParameters:
         data_template = self.model_dump(mode="json")
         data = interpolate_config_strings(data=data_template, variables={"app": app, "workunit": workunit})
-        return SlurmConfig.model_validate(data)
+        return SlurmParameters.model_validate(data)
 
     @classmethod
-    def for_yaml_and_workunit(cls, config_yaml_path: Path, workunit: Workunit) -> SlurmConfigTemplate:
+    def for_yaml_and_workunit(cls, config_yaml_path: Path, workunit: Workunit) -> SlurmParametersTemplate:
         submitter_params = yaml.safe_load(config_yaml_path.read_text())["params"]
         workunit_params = SlurmWorkunitParams.model_validate(workunit.submitter_parameters)
-        return SlurmConfigTemplate(submitter_params=submitter_params, workunit_params=workunit_params)
+        return SlurmParametersTemplate(submitter_params=submitter_params, workunit_params=workunit_params)
 
 
-def evaluate_config(config_yaml_path: Path, workunit: Workunit) -> SlurmConfig:
-    """Evaluates the configuration from the YAML file for a given workunit."""
-    config_template = SlurmConfigTemplate.for_yaml_and_workunit(config_yaml_path, workunit)
+def evaluate_slurm_parameters(config_yaml_path: Path, workunit: Workunit) -> SlurmParameters:
+    """Evaluates the Slurm Parameters from the YAML file for a given workunit."""
+    config_template = SlurmParametersTemplate.for_yaml_and_workunit(config_yaml_path, workunit)
     app_variables = VariablesApp(
         id=workunit.application.id, name=path_safe_name(workunit.application["name"]), version="latest"
     )
