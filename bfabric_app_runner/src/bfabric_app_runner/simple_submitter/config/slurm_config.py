a81f9a7cc865ed6534fa84577808a4c3ed1c36fe
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-28 09:25:59 +0200

    clean

--- bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_config.py
+++ bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_config.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-
+from pathlib import Path  # noqa: TC003
 from functools import cached_property
 
 from bfabric_app_runner.specs.app.app_version import AppVersion  # noqa: TC001
+from bfabric_app_runner.specs.config_interpolation import VariablesApp, VariablesWorkunit, interpolate_config_strings
 from bfabric_app_runner.specs.submitters_spec import SubmitterSlurmSpec  # noqa: TC001
 from bfabric_app_runner.simple_submitter.config.slurm_workunit_params import (
     SlurmWorkunitParams,  # noqa: TC001
@@ -11,18 +12,23 @@ from pydantic import BaseModel
 
 
 class _SlurmConfigBase(BaseModel):
-    submitter_config: SubmitterSlurmSpec
+    submitter_params: SubmitterSlurmSpec
     app_version: AppVersion
-    workunit_config: SlurmWorkunitParams
+    workunit_params: SlurmWorkunitParams
 
 
 class SlurmConfig(_SlurmConfigBase):
     @cached_property
     def sbatch_params(self) -> dict[str, str]:
-        # TODO consistent naming (params vs config)
-        merged = {**self.submitter_config.params, **self.app_version.submitter.params, **self.workunit_config.as_dict()}
+        merged = {**self.submitter_params.params, **self.app_version.submitter.params, **self.workunit_params.as_dict()}
         return {key: value for key, value in merged.items() if value is not None}
 
-    def get_scratch_dir(self) -> str:
-        # TODO check if cast is necessary later
-        return str(self.submitter_config.config.worker_scratch_dir)
+    def get_scratch_dir(self) -> Path:
+        return self.submitter_params.config.worker_scratch_dir
+
+
+class SlurmConfigTemplate(_SlurmConfigBase):
+    def evaluate(self, app: VariablesApp, workunit: VariablesWorkunit) -> SlurmConfig:
+        data_template = self.model_dump(mode="json")
+        data = interpolate_config_strings(data=data_template, variables={"app": app, "workunit": workunit})
+        return SlurmConfig.model_validate(data)
--- bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_config_template.py
+++
@@ -1,9 +0,0 @@
-from bfabric_app_runner.specs.config_interpolation import VariablesApp, interpolate_config_strings, VariablesWorkunit
-from bfabric_app_runner.simple_submitter.config.slurm_config import _SlurmConfigBase, SlurmConfig
-
-
-class SlurmConfigTemplate(_SlurmConfigBase):
-    def evaluate(self, app: VariablesApp, workunit: VariablesWorkunit) -> SlurmConfig:
-        data_template = self.model_dump(mode="json")
-        data = interpolate_config_strings(data=data_template, variables={"app": app, "workunit": workunit})
-        return SlurmConfig.model_validate(data)
