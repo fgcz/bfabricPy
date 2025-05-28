c8b62bd825d3bf54c0361700b9af209edcf02c98
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-28 10:55:26 +0200

    clean

--- bfabric_app_runner/src/bfabric_app_runner/simple_submitter/config/slurm_submitter_params.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-
-from pydantic import BaseModel, StringConstraints
-
-
-class SlurmSubmitterParams(BaseModel):
-    params: dict[Annotated[str, StringConstraints(pattern="^--.*")], str | None]
