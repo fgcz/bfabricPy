bb2e56d26f1a40986a03510a2c98bbb0b9d8edbe
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-06-18 10:55:28 +0200

    adapt test

--- bfabric_scripts/src/bfabric_scripts/bfabric_logthis.py
+++ bfabric_scripts/src/bfabric_scripts/bfabric_logthis.py
@@ -28,7 +28,7 @@ def bfabric_logthis(
 def main() -> None:
     """Parses the command line arguments and calls `bfabric_logthis`."""
     parser = argparse.ArgumentParser(description="log message of external job")
-    parser.add_argument("entity_id", type=int, help="entity id")
+    parser.add_argument("externaljob_id", type=int, help="entity id")
     parser.add_argument("message", type=str, help="message")
     args = vars(parser.parse_args())
     bfabric_logthis(**args)
