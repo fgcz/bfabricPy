4e92e5cf84fa3bf0920a8e27654ee69fd69380b7
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-07-09 15:51:25 +0200

    delete old

--- bfabric_app_runner/examples/file_table/process.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-chunk_dir="$(realpath $1)"
-config_dir=/scratch/leo/code/bfabricPy/bfabric_app_runner/examples/file_table
-uv run --locked --project "$config_dir" \
-  snakemake -p -s "$config_dir/Snakefile" --cores 4 -d "$chunk_dir" outputs.yml
