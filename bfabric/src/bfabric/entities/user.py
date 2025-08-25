e867c61993b905c5d7f31f121c9dc14522801274
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-08-25 11:26:30 +0200

    missing

--- bfabric/src/bfabric/entities/user.py
+++ bfabric/src/bfabric/entities/user.py
@@ -12,3 +12,11 @@ class User(Entity):
 
     def __init__(self, data_dict: dict[str, Any], client: Bfabric | None = None) -> None:
         super().__init__(data_dict=data_dict, client=client)
+
+    @classmethod
+    def find_by_login(cls, login: str, client: Bfabric) -> User | None:
+        """Finds a user by their login name."""
+        users = cls.find_by({"login": login}, client=client)
+        if not users:
+            return None
+        return list(users.values())[0]
