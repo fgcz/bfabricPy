4dc25024e2210e390ff4fee93cd9d38287b37977
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-09-17 15:51:24 +0200

    simplify

--- bfabric_app_runner/src/bfabric_app_runner/inputs/resolve/_common.py
+++ bfabric_app_runner/src/bfabric_app_runner/inputs/resolve/_common.py
@@ -6,18 +6,18 @@ from typing import TYPE_CHECKING
 from bfabric_app_runner.specs.inputs.file_spec import FileSourceSsh, FileSourceSshValue
 
 if TYPE_CHECKING:
-    from bfabric.entities import Resource, Storage
+    from bfabric.entities import Resource
 
 
-def get_file_source_and_filename(
-    resource: Resource, storage: Storage, filename: str | None
-) -> tuple[FileSourceSsh, str]:
-    """Get the file source and filename for a given resource and storage.
+def get_file_source_and_filename(resource: Resource, filename: str | None) -> tuple[FileSourceSsh, str]:
+    """Get the file source and filename for a given resource.
 
     If filename is None, the original filename from the resource is used, otherwise the same value will be returned.
     """
     file_source = FileSourceSsh(
-        ssh=FileSourceSshValue(host=storage["host"], path=f"{storage['basepath']}{resource['relativepath']}")
+        ssh=FileSourceSshValue(
+            host=resource.storage["host"], path=f"{resource.storage['basepath']}{resource['relativepath']}"
+        )
     )
     filename = filename or Path(resource["relativepath"]).name
     return file_source, filename
--- bfabric_app_runner/src/bfabric_app_runner/inputs/resolve/_resolve_bfabric_resource_specs.py
+++ bfabric_app_runner/src/bfabric_app_runner/inputs/resolve/_resolve_bfabric_resource_specs.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from bfabric.entities import Resource, Storage
+from bfabric.entities import Resource
 from bfabric_app_runner.inputs.resolve._common import get_file_source_and_filename
 from bfabric_app_runner.inputs.resolve.resolved_inputs import ResolvedFile
 
@@ -23,20 +23,17 @@ class ResolveBfabricResourceSpecs:
         # Fetch all resources and their storage information in bulk
         resource_ids = [spec.id for spec in specs]
         resources = Resource.find_all(ids=resource_ids, client=self._client)
-        storage_ids = sorted({resource["storage"]["id"] for resource in resources.values()})
-        storages = Storage.find_all(ids=storage_ids, client=self._client)
 
         # Create the file specs
         result = []
         for resource_id, spec in zip(resource_ids, specs):
             resource = resources[resource_id]
-            storage = storages[resource["storage"]["id"]]
-            result.append(self._get_file_spec(spec=spec, resource=resource, storage=storage))
+            result.append(self._get_file_spec(spec=spec, resource=resource))
 
         return result
 
-    def _get_file_spec(self, spec: BfabricResourceSpec, resource: Resource, storage: Storage) -> ResolvedFile:
-        source, filename = get_file_source_and_filename(resource=resource, storage=storage, filename=spec.filename)
+    def _get_file_spec(self, spec: BfabricResourceSpec, resource: Resource) -> ResolvedFile:
+        source, filename = get_file_source_and_filename(resource=resource, filename=spec.filename)
         return ResolvedFile(
             source=source,
             filename=filename,
