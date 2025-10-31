c5522ba25dd7f8867bbab0d4118dae23ff292306
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-10-31 15:12:53 +0100

    dev

--- bfabric/src/bfabric/entities/core/import_entity.py
+++ bfabric/src/bfabric/entities/core/import_entity.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import importlib
 from typing import TypeVar, TYPE_CHECKING
 
+from bfabric.entities.core.generic import GenericEntity
+
 if TYPE_CHECKING:
     from bfabric.entities.core.entity import Entity
 
@@ -13,4 +15,7 @@ def import_entity(entity_class_name: str) -> type[E]:
     """Returns the entity class from a string, e.g. for `"Workunit"` returns the Workunit class."""
     # TODO why is the capitalization needed in the first place? can it be removed?
     capitalized = entity_class_name[0].upper() + entity_class_name[1:]
-    return importlib.import_module(f"bfabric.entities.{entity_class_name.lower()}").__dict__[capitalized]
+    try:
+        return importlib.import_module(f"bfabric.entities.{entity_class_name.lower()}").__dict__[capitalized]
+    except (ModuleNotFoundError, KeyError):
+        return GenericEntity
