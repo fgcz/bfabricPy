5b03c749571ec7602422fb65adc9dcefeaec69cb
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-11-04 11:07:38 +0100

    consistent constructors

--- bfabric/src/bfabric/entities/workflow.py
+++ bfabric/src/bfabric/entities/workflow.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from bfabric.entities.core.entity import Entity
 from bfabric.entities.core.has_container_mixin import HasContainerMixin
@@ -9,16 +9,12 @@ from bfabric.entities.core.has_one import HasOne
 from bfabric.entities.core.user_created_mixin import UserCreatedMixin
 
 if TYPE_CHECKING:
-    from bfabric import Bfabric
     from bfabric.entities.workflowtemplate import WorkflowTemplate
     from bfabric.entities.workflowstep import WorkflowStep
 
 
 class Workflow(Entity, HasContainerMixin, UserCreatedMixin):
     ENDPOINT = "workflow"
-
-    def __init__(self, data_dict: dict[str, Any], client: Bfabric | None = None) -> None:
-        super().__init__(data_dict=data_dict, client=client)
 
     workflow_steps: HasMany[WorkflowStep] = HasMany(entity="WorkflowStep", bfabric_field="workflowstep")
     workflow_template: HasOne[WorkflowTemplate] = HasOne(
