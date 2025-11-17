e370b8b0d4a5715da73f3b60c35c9b0a50c0ca03
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-11-17 12:49:32 +0100

    adapt

--- bfabric/src/bfabric/entities/workunit.py
+++ bfabric/src/bfabric/entities/workunit.py
@@ -28,11 +28,11 @@ class Workunit(Entity, HasContainerMixin, UserCreatedMixin):
     ENDPOINT = "workunit"
 
     application: HasOne[Application] = HasOne(bfabric_field="application")
-    parameters: HasMany[Parameter] = HasMany(entity="Parameter", bfabric_field="parameter", optional=True)
-    resources: HasMany[Resource] = HasMany(entity="Resource", bfabric_field="resource", optional=True)
-    input_resources: HasMany[Resource] = HasMany(entity="Resource", bfabric_field="inputresource", optional=True)
+    parameters: HasMany[Parameter] = HasMany(bfabric_field="parameter", optional=True)
+    resources: HasMany[Resource] = HasMany(bfabric_field="resource", optional=True)
+    input_resources: HasMany[Resource] = HasMany(bfabric_field="inputresource", optional=True)
     input_dataset: HasOne[Dataset] = HasOne(bfabric_field="inputdataset", optional=True)
-    external_jobs: HasMany[ExternalJob] = HasMany(entity="ExternalJob", bfabric_field="externaljob", optional=True)
+    external_jobs: HasMany[ExternalJob] = HasMany(bfabric_field="externaljob", optional=True)
 
     @cached_property
     def application_parameters(self) -> dict[str, str]:
