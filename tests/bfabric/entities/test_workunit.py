3950cdfca2ae2b2120a5a0c7b2189ae9c845b272
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-11-17 12:42:23 +0100

    mostly adapt

--- tests/bfabric/entities/test_workunit.py
+++ tests/bfabric/entities/test_workunit.py
@@ -3,14 +3,11 @@ from typing import Any
 
 import pytest
 
-from bfabric.entities.core.has_many import HasMany
-from bfabric.entities.parameter import Parameter
-from bfabric.entities.resource import Resource
 from bfabric.entities.workunit import Workunit
 
 
 @pytest.fixture()
-def mock_data_dict() -> dict[str, Any]:
+def data_dict() -> dict[str, Any]:
     return {
         "id": 30000,
         "classname": "workunit",
@@ -25,51 +22,59 @@ def mock_data_dict() -> dict[str, Any]:
         "container": {"classname": "project", "id": 3000, "name": "test container"},
         "exportable": "true",
         "parameter": [
-            {"classname": "parameter", "id": 8118},
-            {"classname": "parameter", "id": 8122},
-            {"classname": "parameter", "id": 8119},
+            {"classname": "parameter", "id": 8118, "key": "param8118", "value": "test1", "context": "APPLICATION"},
+            {"classname": "parameter", "id": 8122, "key": "param8122", "value": "test2", "context": "APPLICATION"},
+            {"classname": "parameter", "id": 8119, "key": "param8119", "value": "test3", "context": "SUBMITTER"},
         ],
         "status": "AVAILABLE",
+        "resource": [
+            {"classname": "resource", "id": 1000, "name": "my resource"},
+        ],
+        "inputresource": [
+            {"classname": "resource", "id": 500, "name": "input resource"},
+        ],
     }
 
 
 @pytest.fixture()
-def mock_workunit(mock_data_dict: dict[str, Any], mock_client, bfabric_instance) -> Workunit:
-    return Workunit(mock_data_dict, client=mock_client, bfabric_instance=bfabric_instance)
+def workunit(data_dict, mock_client, bfabric_instance) -> Workunit:
+    return Workunit(data_dict, client=mock_client, bfabric_instance=bfabric_instance)
 
 
-def test_data_dict(mock_workunit: Workunit, mock_data_dict: dict[str, Any]) -> None:
-    assert mock_workunit.data_dict == mock_data_dict
-    assert mock_workunit.data_dict is not mock_data_dict
+def test_data_dict(workunit: Workunit, data_dict) -> None:
+    assert workunit.data_dict == data_dict
+    assert workunit.data_dict is not data_dict
 
 
-def test_parameters(mocker, mock_workunit) -> None:
-    get = mocker.patch.object(HasMany, "__get__")
-    assert mock_workunit.parameters == get.return_value
-    assert get.call_args[0][0]._entity_type == Parameter
-    assert get.call_args[0][0]._bfabric_field == "parameter"
-    assert get.call_args[0][0]._ids_property is None
+def test_parameters(workunit) -> None:
+    # TODO drop sorting?
+    assert workunit.parameters.ids == [8118, 8122, 8119]
 
 
-def test_resources(mocker, mock_workunit) -> None:
-    get = mocker.patch.object(HasMany, "__get__")
-    assert mock_workunit.resources == get.return_value
-    assert get.call_args[0][0]._entity_type == Resource
-    assert get.call_args[0][0]._bfabric_field == "resource"
-    assert get.call_args[0][0]._ids_property is None
+def test_application_parameters(workunit) -> None:
+    assert workunit.application_parameters == {
+        "param8118": "test1",
+        "param8122": "test2",
+    }
 
 
-def test_input_resources(mocker, mock_workunit) -> None:
-    get = mocker.patch.object(HasMany, "__get__")
-    assert mock_workunit.input_resources == get.return_value
-    assert get.call_args[0][0]._entity_type == Resource
-    assert get.call_args[0][0]._bfabric_field == "inputresource"
-    assert get.call_args[0][0]._ids_property is None
+def test_submitter_parameters(workunit) -> None:
+    assert workunit.submitter_parameters == {
+        "param8119": "test3",
+    }
 
 
-def test_parameter_values(mocker, mock_workunit: Workunit) -> None:
+def test_resources(workunit) -> None:
+    assert workunit.resources.ids == [1000]
+
+
+def test_input_resources(workunit) -> None:
+    assert workunit.input_resources.ids == [500]
+
+
+def test_parameter_values(mocker, workunit: Workunit) -> None:
     mocker.patch.object(
-        mock_workunit,
+        workunit,
         "parameters",
         [
             mocker.MagicMock(key="key1", value="value1", __getitem__=lambda _self, x: {"context": "APPLICATION"}[x]),
@@ -77,26 +82,24 @@ def test_parameter_values(mocker, mock_workunit: Workunit) -> None:
             mocker.MagicMock(key="key3", value="value3", __getitem__=lambda _self, x: {"context": "SUBMITTER"}[x]),
         ],
     )
-    assert mock_workunit.application_parameters == {"key1": "value1", "key2": "value2"}
-    assert mock_workunit.submitter_parameters == {"key3": "value3"}
+    assert workunit.application_parameters == {"key1": "value1", "key2": "value2"}
+    assert workunit.submitter_parameters == {"key3": "value3"}
 
 
-def test_container_when_project(mock_workunit) -> None:
-    mock_workunit._data_dict["container"]["classname"] = "project"
-    assert mock_workunit.container.classname == "project"
-    assert mock_workunit.container.id == 3000
+def test_container_when_project(workunit) -> None:
+    workunit._data_dict["container"]["classname"] = "project"
+    assert workunit.container.classname == "project"
+    assert workunit.container.id == 3000
 
 
-def test_container_when_order(mock_workunit) -> None:
-    mock_workunit._data_dict["container"]["classname"] = "order"
-    assert mock_workunit.container.classname == "order"
-    assert mock_workunit.container.id == 3000
+def test_container_when_order(workunit) -> None:
+    workunit._data_dict["container"]["classname"] = "order"
+    assert workunit.container.classname == "order"
+    assert workunit.container.id == 3000
 
 
-def test_store_output_folder(mock_workunit) -> None:
-    assert (
-        Path("xyz3000/bfabric/Tech/my_app/2024/2024-01/2024-01-02/workunit_30000") == mock_workunit.store_output_folder
-    )
+def test_store_output_folder(workunit) -> None:
+    assert Path("xyz3000/bfabric/Tech/my_app/2024/2024-01/2024-01-02/workunit_30000") == workunit.store_output_folder
 
 
 def test_repr() -> None:
