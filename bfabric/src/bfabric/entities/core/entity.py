dc1ac2367dcc8eba76a0df138c4a0cb68b37bcaf
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2025-11-28 08:45:14 +0100

    Custom attributes utilities (#355)

--- bfabric/src/bfabric/entities/core/entity.py
+++ bfabric/src/bfabric/entities/core/entity.py
@@ -82,6 +82,19 @@ class Entity(FindMixin):
         return References(client=self._client, bfabric_instance=self.__bfabric_instance, data_ref=self.__data_dict)
 
     @property
+    def custom_attributes(self) -> dict[str, str]:
+        """Returns custom attributes as a dictionary, if the entity has any.
+
+        If the field exists but is empty, an empty dictionary is returned.
+        If the field does not exist, an `AttributeError` is raised.
+        """
+        if "customattribute" not in self.__data_dict:
+            msg = f"Entity of classname '{self.classname}' has no custom attributes."
+            raise AttributeError(msg)
+
+        return {attr["name"]: attr["value"] for attr in self.__data_dict["customattribute"]}
+
+    @property
     def _client(self) -> Bfabric | None:
         """Returns the client associated with the entity."""
         return self.__client
---
+++ bfabric/src/bfabric/experimental/update_custom_attributes.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from bfabric import Bfabric
+    from bfabric.entities.core.uri import EntityUri
+    from bfabric.results.result_container import ResultContainer
+
+
+def update_custom_attributes(
+    client: Bfabric, entity_uri: EntityUri | str, custom_attributes: dict[str, str], *, replace: bool = False
+) -> ResultContainer:
+    """Updates the custom attributes of the specified entity.
+
+    NOTE: Use in cache contexts may lead to unwanted consequences, if custom attributes are updated by a different
+    application.
+
+    :param client: Bfabric client
+    :param entity_uri: Entity URI
+    :param custom_attributes: Custom attributes to update
+    :param replace: Replace existing custom attributes mapping, or update/add only the specified ones.
+    """
+    # read the current custom attributes (if requested)
+    if replace:
+        attributes_dict = {}
+    else:
+        entity = client.reader.read_uri(entity_uri)
+        attributes_dict = entity.custom_attributes
+    # merge new args
+    attributes_dict.update(custom_attributes)
+    # write the result
+    attributes_list = [{"name": key, "value": value} for key, value in attributes_dict.items()]
+    return client.save(
+        entity_uri.components.entity_type, {"id": entity_uri.components.entity_id, "customattribute": attributes_list}
+    )
