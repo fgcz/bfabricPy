53cddc4e878715adbc0714b433c34d504d93fbe6
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-12-10 10:17:40 +0100

    typing iteration

--- bfabric/src/bfabric/engine/types.py
+++ bfabric/src/bfabric/engine/types.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 import datetime
+from collections.abc import Mapping
 
 # Request types (data sent TO the API)
 ApiRequestScalarType = int | float | str | bool | datetime.datetime | None
-ApiRequestDataType = ApiRequestScalarType | list["ApiRequestDataType"] | dict[str, "ApiRequestDataType"]
-ApiRequestObjectType = dict[str, ApiRequestDataType]
+ApiRequestDataType = ApiRequestScalarType | list["ApiRequestDataType"] | Mapping[str, "ApiRequestDataType"]
+ApiRequestObjectType = Mapping[str, ApiRequestDataType]
 
 # Response types (data received FROM the API)
 ApiResponseScalarType = int | float | str | bool | None
