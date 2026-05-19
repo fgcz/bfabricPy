f2d13b53fc251f274e7c6da3d112d714ef05fb90
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2026-05-19 08:58:48 +0200

    Testing

--- tests/bfabric/operations/dataset/test_validation.py
+++ tests/bfabric/operations/dataset/test_validation.py
@@ -1,7 +1,8 @@
 import polars as pl
+import pytest
 from logot import Logot, logged
 
-from bfabric.operations.dataset import warn_on_trailing_spaces
+from bfabric.operations.dataset import check_for_invalid_characters, warn_on_trailing_spaces
 
 
 def test_warn_on_trailing_spaces_when_no(logot: Logot):
@@ -17,25 +18,37 @@ def test_warn_on_trailing_spaces_when_warning(logot):
 
 
 def test_check_for_invalid_characters_clean():
-    from bfabric.operations.dataset import check_for_invalid_characters
-
     table = pl.DataFrame({"a": ["a", "b"], "b": [1, 2]})
     check_for_invalid_characters(table, ",\t")
 
 
 def test_check_for_invalid_characters_dirty():
-    import pytest
-
-    from bfabric.operations.dataset import check_for_invalid_characters
-
     table = pl.DataFrame({"a": ["x,y", "b"], "b": ["fine", "ok"]})
-    with pytest.raises(RuntimeError, match="Invalid characters found in columns:"):
+    with pytest.raises(RuntimeError, match=r"Invalid characters found in columns: \['a'\]"):
         check_for_invalid_characters(table, ",")
+
+
+def test_check_for_invalid_characters_multiple_columns():
+    table = pl.DataFrame(
+        {
+            "col1": ["abc", "d!ef", "ghi"],
+            "col2": ["123", "45@6", "789"],
+            "col3": ["xyz", "uvw", "rst"],
+        }
+    )
+    with pytest.raises(RuntimeError, match=r"Invalid characters found in columns: \['col1', 'col2'\]"):
+        check_for_invalid_characters(table, "!@#")
 
 
 def test_check_for_invalid_characters_empty_chars_skips():
     table = pl.DataFrame({"a": ["x,y", "b"]})
-    # empty invalid_characters should not raise
-    from bfabric.operations.dataset import check_for_invalid_characters
-
     check_for_invalid_characters(table, "")
+
+
+def test_check_for_invalid_characters_empty_dataframe():
+    check_for_invalid_characters(pl.DataFrame(), "!@#")
+
+
+def test_check_for_invalid_characters_non_string_columns():
+    table = pl.DataFrame({"col1": [1, 2, 3], "col2": [4.5, 5.6, 6.7], "col3": ["abc", "def", "ghi"]})
+    check_for_invalid_characters(table, "!@#")
